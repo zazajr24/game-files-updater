87668bfdd9897d29ad87dbb0013b0b98d00ad631
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:55:35 +0200

    Update 2169200.lua

---
+++ Manifests/Sniper Elite_ Resistance Manifests and Keys/2169200.lua
@@ -0,0 +1,52 @@
+-- 2169200's Lua and Manifest Created by Hubcap Manifest
+-- Sniper Elite: Resistance
+-- Created: September 30, 2025 at 17:58:16 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 24
+-- Total DLCs: 10
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2169200) -- Sniper Elite: Resistance
+-- MAIN APP DEPOTS
+addappid(2169201, 1, "f0442f7884739333bfdcc7544a60a639ccf65a344fb7e5312d81b71d53f3483f") -- Depot 2169201
+addappid(2169202, 1, "01734f2ec3ffff33c42ef6f7337642950f08f3d7251d72eead052011085536f6") -- Depot 2169202
+addappid(2169203, 1, "e6c06bbeb1a3f7316704546bd26db1ec93406963a3d84d402a9e31956f8b7cc7") -- Depot 2169203
+addappid(2169204, 1, "d11132ae9b8705ec81f29311b269dda0811ffb55d25dbf766eadaac3660ad7f5") -- Depot 2169204
+addappid(2169205, 1, "cfdf61799a0c9e8ec83eb607ab4fd3482ee99d700d99eb6d6d194f0f41360faa") -- Depot 2169205
+addappid(2169206, 1, "58856b5fdc9f0115eeef7fa218522ef3f74c223a794906e53b01541c82b9f32e") -- Depot 2169206
+addappid(2169207, 1, "f2266e1c1d7f21bcf969ad780c66debeaf32eb3d564ed083458f9d64ef89d1c6") -- Depot 2169207
+addappid(2169208, 1, "7130b3aa6ad5e700afca9674a124616dc9d04c226ebab6453d98cab4d8dfa2d1") -- Depot 2169208
+addappid(2169209, 1, "d722416d284d1778f0911b4c0577c3e6864b72100d39bdea21056ade0129251b") -- Depot 2169209
+addappid(2561511, 1, "ed396bd5a04dcb8cbf85bfb20018ce8905c58a3f245459ca2c4f18f036a5d932") -- Depot 2561511
+addappid(2561512, 1, "edd809a64000a91fb3ac08691ba68bbcb629ea6f693c9811c3c32679ce936be9") -- Depot 2561512
+addappid(2561513, 1, "fc8eca9e12e0f3bf1fb24524a1703fcc77a1f3f636afe8ac954922d202d3989d") -- Depot 2561513
+addappid(2561514, 1, "81aa2260b9d0d8a0a4e194dee301d61e683d6aa6aa151e20ad351f579b9ce991") -- Depot 2561514
+addappid(2561515, 1, "db078358a476eb1b8549563936329da856dcdf7f2dc1b5deeec74eaf78d679c6") -- Depot 2561515
+addappid(2561516, 1, "1c448e10003fbe6f2be8716ca09189c53b2bd8dbfc36e6abe243d7736679a458") -- Depot 2561516
+addappid(2561517, 1, "444cee8f5e54fc5971ae18ec553fd532307d6ba3b9d11036cc8a143b36f32f38") -- Depot 2561517
+addappid(2561518, 1, "6439c73ba8f70236c8e73c167af4b04bbc5f2476ffab4554ccc1340e466a4c2d") -- Depot 2561518
+addappid(2561519, 1, "6a940a58a5b3e5fe4b41c87e7d1e8fe5339025a1934e46835f0146b2f2a4e0ab") -- Depot 2561519
+addappid(2561521, 1, "9f465c45d74c657d4af3adf7d4048c896868b411ec45d6dde4cc057d5fb0c7ae") -- Depot 2561521
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sniper Elite Resistance - Target Fhrer - Lights, Camera, Achtung (AppID: 2561520)
+addappid(2561520)
+addappid(2561520, 1, "e9782dcff591c25264d47b0a83c5d9738ae22a2456e4d7fd5eba2ce17858d21c") -- Sniper Elite Resistance - Target Fhrer - Lights, Camera, Achtung - Depot 2561520
+-- Sniper Elite Resistance - Vercors Vendetta Mission and Weapons Pack (AppID: 2729150)
+addappid(2729150)
+addappid(2729150, 1, "9008f4414e442c17ceb0d7b181d9da5a763bb6407bef14f212f915e57c7e921c") -- Sniper Elite Resistance - Vercors Vendetta Mission and Weapons Pack - Depot 2729150
+-- Sniper Elite Resistance - Striking Range Mission, Weapons and Skins Pack (AppID: 2800240)
+addappid(2800240)
+addappid(2800240, 1, "28250feeef899032c0de5ae470b5a9b880ec93ddfd6afe2ed52ed6604ae09ea9") -- Sniper Elite Resistance - Striking Range Mission, Weapons and Skins Pack - Depot 2800240
+-- Sniper Elite Resistance - Mud and Thunder Mission, Weapons and Skin Pack (AppID: 2800250)
+addappid(2800250)
+addappid(2800250, 1, "27f94daae928259b037667abc464aaa7cac0901503e2fea07fe5e7abc114468d") -- Sniper Elite Resistance - Mud and Thunder Mission, Weapons and Skin Pack - Depot 2800250
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3003600) -- Sniper Elite Resistance - M1911 pistol
+addappid(3003610) -- Sniper Elite Resistance - Karabiner 98 Rifle
+addappid(3003620) -- Sniper Elite Resistance Fairburnes Armoury Weapons and Skins Pack
+addappid(3003630) -- Sniper Elite Resistance - Commando Weapon and Skin Pack
+addappid(3258690) -- Sniper Elite Resistance Season Pass
+addappid(3618300) -- Sniper Elite Resistance - Wrath of the Resistance Weapon and Skins Pack
