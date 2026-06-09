660201f8092317ee633c6e9f9d521951c4827b01
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:47:21 +0200

    Update 1693980.lua

---
+++ Manifests/Dead Space 2023 Manifests and Keys/1693980.lua
@@ -0,0 +1,16 @@
+-- 1693980's Lua and Manifest Created by Hubcap Manifest
+-- Dead Space
+-- Created: September 29, 2025 at 06:56:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1693980) -- Dead Space
+-- MAIN APP DEPOTS
+addappid(1693981, 1, "9738ac7c4b941978165b403d5785d6c8c331366f7feb30c0b276401fd71f4ccb") -- Depot 1693981
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2164210) -- Dead Space Digital Deluxe Edition Upgrade
