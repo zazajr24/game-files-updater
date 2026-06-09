8da318521c31ce5f8862dc669a7df8cd150e01d6
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:58:09 +0200

    Update 1570010.lua

---
+++ Manifests/FAR_ Changing Tides Manifests and Keys/1570010.lua
@@ -0,0 +1,11 @@
+-- 1570010's Lua and Manifest Created by Hubcap Manifest
+-- FAR: Changing Tides
+-- Created: December 09, 2025 at 06:40:29 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1570010) -- FAR: Changing Tides
+-- MAIN APP DEPOTS
+addappid(1570011, 1, "f3aa74143ab21ae7aed97c2ac67b8c6510b94a84a41f44157671da176d86303e") -- Depot 1570011
