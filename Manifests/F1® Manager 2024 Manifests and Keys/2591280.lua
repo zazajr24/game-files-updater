4f3db44b09826a60028173535bbd021819987e54
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:42:46 +0200

    Update 2591280.lua

---
+++ Manifests/F1® Manager 2024 Manifests and Keys/2591280.lua
@@ -0,0 +1,19 @@
+-- 2591280's Lua and Manifest Created by Hubcap Manifest
+-- F1® Manager 2024
+-- Created: September 29, 2025 at 11:16:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 4
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2591280) -- F1® Manager 2024
+-- MAIN APP DEPOTS
+addappid(2591281, 1, "e371e0b7a49da6cdcd910edd1f1f9f7eaa93de33884db50e3778ec73f34800b2") -- Depot 2591281
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2934780) -- F1 Manager 2024 - Prepurchase Bonus
+addappid(2934790) -- F1 Manager 2024 - Deluxe Upgrade Pack
+addappid(3205030) -- F1 Manager 2024 Historical Livery Pack
+addappid(3205040) -- F1 Manager 2024 Abstract Livery Pack
