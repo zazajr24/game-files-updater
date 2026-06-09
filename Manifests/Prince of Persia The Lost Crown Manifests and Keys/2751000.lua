93ebbea8a905981a2934f865d0f1dcc99b968621
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:41:32 +0200

    Update 2751000.lua

---
+++ Manifests/Prince of Persia The Lost Crown Manifests and Keys/2751000.lua
@@ -0,0 +1,30 @@
+-- 2751000's Lua and Manifest Created by Hubcap Manifest
+-- Prince of Persia The Lost Crown
+-- Created: June 05, 2026 at 21:08:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 13
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2751000, 1, "edd7db29fe179c620b586ac9fb6f168b905644f7f8d08008b79dfe360cfab3dd") -- Prince of Persia The Lost Crown
+-- MAIN APP DEPOTS
+addappid(2751001, 1, "3570b20701e5a605870438400f373ede72ba4b0d20d00fedabb48ac6a5158136") -- Depot 2751001
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2961560) -- Prince of Persia The Lost Crown - Ubisoft Activation
+addappid(2961570) -- Prince of Persia The Lost Crown - Deluxe Pack
+addappid(2961580) -- Prince of Persia The Lost Crown - Deluxe Pack Ubisoft Activation
+addappid(2961630) -- Prince of Persia The Lost Crown - Deluxe Edition Ubisoft Activation
+addappid(3102580) -- Prince of Persia The Lost Crown - Mask of Darkness
+addappid(3102590) -- Prince of Persia The Lost Crown - Mask of Darkness Ubisoft Activation
+addappid(3102600) -- Prince of Persia The Lost Crown - Two Thrones Costume
+addappid(3102610) -- Prince of Persia The Lost Crown - Two Thrones Costume Ubisoft Activation
+addappid(3102620) -- Prince of Persia The Lost Crown - Dark Prince Costume
+addappid(3102630) -- Prince of Persia The Lost Crown - Dark Prince Costume Ubisoft Activation
+addappid(3104180) -- Prince of Persia The Lost Crown - Complete Edition Ubisoft Activation
+addappid(3140380) -- Prince of Persia The Lost Crown  Complete Upgrade pack
+addappid(3140390) -- Prince of Persia The Lost Crown - Complete Upgrade Pack Ubisoft Activation
