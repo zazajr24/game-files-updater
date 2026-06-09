3054dc4060828d8018f7d6fed11bb43a53998a17
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:46:22 +0200

    Update 3230400.lua

---
+++ Manifests/EA SPORTS™ Madden NFL 26 Manifests and Keys/3230400.lua
@@ -0,0 +1,19 @@
+-- 3230400's Lua and Manifest Created by Hubcap Manifest
+-- EA SPORTS™ Madden NFL 26
+-- Created: June 05, 2026 at 05:55:28 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3230400, 1, "17e11ffc2538da63676717d1a125a5b5b07132a14006620a769fbc691ecfc6aa") -- EA SPORTS™ Madden NFL 26
+-- MAIN APP DEPOTS
+addappid(3230401, 1, "d27469f0328400d0e61683059a3ad3824df4c1c1ae36fe6933dcfb093d01440a") -- Depot 3230401
+addappid(3230402, 1, "82076c00a744f17dab06a0c6b804a4be4e36589ceff9c4a6cd31bba837e1e203") -- Depot 3230402
+-- SHARED DEPOTS (from other apps)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") -- Depot 3893181 (Shared from App 3893180)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3415740) -- Madden NFL 26 Standard Edition Key
+addappid(4681450) -- Madden NFL 27 Deluxe Edition - M26 Pre-Purchase Content
