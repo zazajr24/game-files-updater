3d277828774f037c04f8885d50c123d47282471d
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:42:49 +0200

    Update 3357650.lua

---
+++ Manifests/PRAGMATA Manifests and Keys/3357650.lua
@@ -0,0 +1,24 @@
+-- 3357650's Lua and Manifest Created by Hubcap Manifest
+-- PRAGMATA
+-- Created: April 17, 2026 at 00:05:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 2
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3357650, 1, "18e75bfb3f8c26b7a59d9e3a7013e74e1c59fe563aa00ee15f54a35b17691766") -- PRAGMATA
+-- MAIN APP DEPOTS
+addappid(3357651, 1, "18e75bfb3f8c26b7a59d9e3a7013e74e1c59fe563aa00ee15f54a35b17691766") -- Depot 3357651
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- PRAGMATA Outfit Set - Neo Bushido  Neo Kunoichi (AppID: 3859920)
+addappid(3859920)
+addappid(3859920, 1, "1fb811843160e2a11211f5bba90ece0ef2dac5994c19936e50ff85faf2f80423") -- PRAGMATA Outfit Set - Neo Bushido  Neo Kunoichi - Depot 3859920
+-- PRAGMATA Shelter Variety Pack (AppID: 3859930)
+addappid(3859930)
+addappid(3859930, 1, "14001799a6877a14751af43adbd59ca3cc037cf202aa5615e0c70e057090ddce") -- PRAGMATA Shelter Variety Pack - Depot 3859930
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3357652) -- Depot 3357652 (empty depot)
+-- addappid(4093670) -- Depot 4093670 (empty depot)
