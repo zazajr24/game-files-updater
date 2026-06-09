ebfc7b8275fb63c2b659773ceb21a66fe93b90b3
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:47:12 +0200

    Update 2624870.lua

---
+++ Manifests/Life is Strange_ Reunion Manifests and Keys/2624870.lua
@@ -0,0 +1,25 @@
+-- 2624870's Lua and Manifest Created by Hubcap Manifest
+-- Life is Strange: Reunion
+-- Created: April 15, 2026 at 13:19:18 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 3
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2624870, 1, "fd31359f1520ec7daf54c757cf4c13f8d10cf64d0961b01521923dd5fd8ef7fe") -- Life is Strange: Reunion
+-- MAIN APP DEPOTS
+addappid(2624871, 1, "b639c25fe08af0a3e5d2f68d6f80972e9bacf8df4910d3f0a3ec734133ce62b6") -- Depot 2624871
+-- SHARED DEPOTS (from other apps)
+addappid(228982, 1, "fa2997f03c3576c41174f200596ab2246fd1e39c0911f9b869e92635ae5b0ff5") -- VC 2008 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Life is Strange Reunion - DLC - BTS Documentary Video (AppID: 3977740)
+addappid(3977740)
+addappid(3977740, 1, "840a10096e49e6bd28961b6e7cd53a86cb6acb0f7a819787844096e5c7fbac85") -- Life is Strange Reunion - DLC - BTS Documentary Video - Depot 3977740
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3977730) -- Life is Strange Reunion - DLC - Classic Outfits
+addappid(4340990) -- Life is Strange Reunion Digital Deluxe upgrade
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3977730) -- Depot 3977730 (empty depot)
