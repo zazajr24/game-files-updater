14d3f35f8603979a3935447c9455668b93341f01
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:39:55 +0200

    Update 1307710.lua

---
+++ Manifests/GRID Legends Manifests and Keys/1307710.lua
@@ -0,0 +1,31 @@
+-- 1307710's Lua and Manifest Created by Hubcap Manifest
+-- GRID Legends
+-- Created: September 29, 2025 at 15:34:48 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 9
+-- Total DLCs: 9
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1307710) -- GRID Legends
+-- MAIN APP DEPOTS
+addappid(1307711, 1, "60b113c0bab9f60878b8f651772e080a6b1f3ec17ab744fc73bd6231333bd405") -- Depot 1307711
+addappid(1307712, 1, "0ba8d9b81078ca25cc230006282e584220ec3cfbe464952aed5f4419129f9dfe") -- Depot 1307712
+addappid(1307713, 1, "511b87ae7f5570dc0f291b454510aff69b900489cf96dc8483380eaa3dfd25aa") -- Depot 1307713
+addappid(1307715, 1, "20864623f4ca814d91f55495e3b329f05594ea804851d28f88df7eb81c2f8425") -- Depot 1307715
+addappid(1307716, 1, "fc4c2999bba1c8617d347556400a1263e3c97e4e5b683a56984ae3450f4ac795") -- Depot 1307716
+addappid(1307717, 1, "3b5710bbf8a9042c4850d166cea5a90e57407d49d8f70e0cc14b25c473166540") -- Depot 1307717
+addappid(1307718, 1, "1a044e0cabda05f5a5bc6b3b06c61952a842a56469c3aa6ccc109c922f985fbd") -- Depot 1307718
+addappid(1307719, 1, "905886c285e11db9316f59d20498dc8bf7436aef17f4ceb1cabc0ccaf488ec3e") -- Depot 1307719
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1745470) -- GRID Legends - Deluxe Upgrade
+addappid(1745471) -- GRID Legends Mechanic Pass
+addappid(1745472) -- GRID Legends Voltz Pack
+addappid(1745473) -- GRID Legends - Pre-Order Content
+addappid(1745474) -- GRID Legends Valentins Classic Car-Nage
+addappid(1745475) -- GRID Legends Enduring Spirit
+addappid(1745476) -- GRID Legends Rise of Ravenwest
+addappid(1745477) -- GRID Legends Winter Bash
+addappid(1918160) -- GRID Legends - EA Play Trial
