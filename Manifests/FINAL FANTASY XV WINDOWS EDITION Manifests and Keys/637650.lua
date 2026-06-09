65827ae03f98145cf31be2838895455d8bafbf11
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:43:48 +0200

    Update 637650.lua

---
+++ Manifests/FINAL FANTASY XV WINDOWS EDITION Manifests and Keys/637650.lua
@@ -0,0 +1,28 @@
+-- 637650's Lua and Manifest Created by Hubcap Manifest
+-- FINAL FANTASY XV WINDOWS EDITION
+-- Created: September 29, 2025 at 12:16:10 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 5
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(637650, 1, "adf34833b6a8198d4e2bab959715d783ea4553a321b5c1289834047aadcbc540") -- FINAL FANTASY XV WINDOWS EDITION
+-- MAIN APP DEPOTS
+addappid(637651, 1, "067cfbc8b2cdd98dec9619de67a9c50b91477b19e8220fe19adf16631383d1e6") -- FINAL FANTASY XV Content
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- FFXV WINDOWS EDITION 4K Resolution Pack (AppID: 771560)
+addappid(771560)
+addappid(771561, 1, "1a825705887e4f8fe89c25a9c508cfcf4b7e8040bf8eae935def11aa8f5cdc7f") -- FFXV WINDOWS EDITION 4K Resolution Pack - FINAL FANTASY XV Bonus texture pack depot
+-- FFXV Fashion Collection (AppID: 787600)
+addappid(787600)
+addtoken(787600, "1694990638127720653")
+addappid(787601, 1, "ff76f9f0a4ff7922c4c28b8d6726cf67791c1558e5c7aafd390eaf8f0051fbf9") -- FFXV Fashion Collection - Depot 787601
+-- FINAL FANTASY XV EPISODE ARDYN (AppID: 977610)
+addappid(977610)
+addappid(977611, 1, "d7ea2d4a26e6b83babdec6daf4fb919b775deafb8768cb8a55125a754bf558cb") -- FINAL FANTASY XV EPISODE ARDYN - FFXV DLC A depot
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(787610) -- FFXV 8700K Pack
+addappid(922570) -- FFXV Alien Shield
