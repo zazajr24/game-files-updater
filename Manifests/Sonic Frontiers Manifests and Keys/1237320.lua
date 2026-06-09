fb0da646325f22caa08316a5afda1f9157b31a6e
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:41:46 +0200

    Update 1237320.lua

---
+++ Manifests/Sonic Frontiers Manifests and Keys/1237320.lua
@@ -0,0 +1,33 @@
+-- 1237320's Lua and Manifest Created by Hubcap Manifest
+-- Sonic Frontiers
+-- Created: September 30, 2025 at 18:20:46 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1237320) -- Sonic Frontiers
+-- MAIN APP DEPOTS
+addappid(1237321, 1, "9c57e01f36d7bf7a77b07678b08a0e690d785dfbcd9a421ae1fc2157741b5ccd") -- Depot 1237321
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Sonic Frontiers Explorers Treasure Box (AppID: 2079301)
+addappid(2079301)
+addappid(2079301, 1, "db37953f414051736b6e659e9146fa2e081148a94633fbea635610f79d6588da") -- Sonic Frontiers Explorers Treasure Box - Depot 2079301
+-- Sonic Frontiers Monster Hunter Collaboration Pack (AppID: 2089730)
+addappid(2089730)
+addappid(2089730, 1, "dfb354863019c75a8143f9e7073ed589df096057283247fe5e16c1c6225667f2") -- Sonic Frontiers Monster Hunter Collaboration Pack - Depot 2089730
+-- Sonic Frontiers Holiday Cheer Suit (AppID: 2089733)
+addappid(2089733)
+addappid(2089733, 1, "3dedee464853a2702ce024319deb87d57c6e2e404a328bce05f71b18a2649faf") -- Sonic Frontiers Holiday Cheer Suit - Depot 2089733
+-- Sonic Frontiers Sonic Adventure 2 Shoes (AppID: 2089840)
+addappid(2089840)
+addappid(2089840, 1, "cf0ee3bde87a208b2bf9425b695b287a2997de71728df89f826fca1667197f85") -- Sonic Frontiers Sonic Adventure 2 Shoes - Depot 2089840
+-- Sonic Frontiers Digital Art Book with Mini Digital Soundtrack (AppID: 2093240)
+addappid(2093240)
+addappid(2093241, 1, "9eac17d35c7a4d92c2bc39bbfcd5a64c61df1d4aed6c5dc7bad49f962d4780bf") -- Sonic Frontiers Digital Art Book with Mini Digital Soundtrack - Depot 2093241
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2079300) -- Sonic Frontiers Adventurers Treasure Box
