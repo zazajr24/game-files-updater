f3d6ff1b62eed3e3f737e584325090d470d451de
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:41:22 +0200

    Update 703080.lua

---
+++ Manifests/Planet Zoo Manifests and Keys/703080.lua
@@ -0,0 +1,41 @@
+-- 703080's Lua and Manifest Created by Hubcap Manifest
+-- Planet Zoo
+-- Created: May 28, 2026 at 12:50:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 22
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(703080, 1, "519fa2ea9c9a1cca0bbd6cd0c7a7f8c220812ec10ef3f7402764d03bc6e8121c") -- Planet Zoo
+-- MAIN APP DEPOTS
+addappid(703081, 1, "bc33ccdea0df8610185dab43ef1f141daecf265e379acb2feb58fde38212796c") -- Darwin Content
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Deluxe Edition Extras (AppID: 1098120)
+addappid(1098120)
+addappid(1098120, 1, "9decae9ea2fe2d9055772fecad34b56835f0464dff8d4159d7457fc3586d2994") -- Deluxe Edition Extras - Deluxe Edition Extras (1098120) Depot
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1098121) -- Deluxe Edition Animals
+addtoken(1098121, "3634592998398443903")
+addappid(1196770) -- Planet Zoo Arctic Pack
+addappid(1238440) -- Planet Zoo South America Pack
+addappid(1349400) -- Planet Zoo Australia Pack
+addappid(1471590) -- Planet Zoo Aquatic Pack
+addappid(1567110) -- Planet Zoo Southeast Asia Animal Pack
+addappid(1647620) -- Planet Zoo Africa Pack
+addappid(1726150) -- Planet Zoo Europe Pack
+addappid(1747960) -- Planet Zoo North America Animal Pack
+addappid(1934140) -- Planet Zoo Wetlands Animal Pack
+addappid(2013290) -- Planet Zoo Conservation Pack
+addappid(2150050) -- Planet Zoo Twilight Pack
+addappid(2199210) -- Planet Zoo Grasslands Animal Pack
+addappid(2346830) -- Planet Zoo Tropical Pack
+addappid(2436600) -- Planet Zoo Arid Animal Pack
+addappid(2502240) -- Planet Zoo Oceania Pack
+addappid(2675740) -- Planet Zoo Eurasia Animal Pack
+addappid(2837730) -- Planet Zoo Barnyard Animal Pack
+addappid(3146420) -- Planet Zoo Zookeepers Animal Pack
+addappid(3473820) -- Planet Zoo Americas Animal Pack
+addappid(3586990) -- Planet Zoo Asia Animal Pack
