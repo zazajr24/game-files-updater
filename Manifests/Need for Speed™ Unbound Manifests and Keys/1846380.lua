203625714ff11c363e7b2be9533f4e3bb96c546b
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:56:47 +0200

    Update 1846380.lua

---
+++ Manifests/Need for Speed™ Unbound Manifests and Keys/1846380.lua
@@ -0,0 +1,60 @@
+-- 1846380's Lua and Manifest Created by Hubcap Manifest
+-- Need for Speed™ Unbound
+-- Created: June 04, 2026 at 14:26:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 16
+-- Total DLCs: 31
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1846380, 1, "4caf2ec5251ba3ba0ad87e79a2454a1b60ebfb24cce75437b28ccbfa29ed3d88") -- Need for Speed™ Unbound
+-- MAIN APP DEPOTS
+addappid(1846381, 1, "372b477136e77527ae6282aec9dcfba367e4e90e48215dbd4b4a5339ee3f2ef8") -- Depot 1846381
+addappid(1846382, 1, "0eb1e6b3c406f906eed92b706c0cb931fc17e77b9c082b431dbbd62156dd8f71") -- Depot 1846382
+addappid(1846383, 1, "400757a8e3e7ba4631a388d7296a60db34d98d8df5ab76b1e3602aa7d539b1ee") -- Depot 1846383
+addappid(1846384, 1, "64951a96b3ad56152eab790ae5639d318505d2d144dd223f4053470bff6c4e87") -- Depot 1846384
+addappid(1846385, 1, "f5d785ddf95417ede5d45431fbb8c3d9e871bc346b0a946afa35414a342ee737") -- Depot 1846385
+addappid(1846386, 1, "ee61c86ff876c1bde9e1360a684c6d061dea8f255bd07324450db35f42258e12") -- Depot 1846386
+addappid(1846387, 1, "5e7f5a9d797728d25c13e36756ca53450369fd17a2a37bb2f17bf345061dcdc5") -- Depot 1846387
+addappid(1846388, 1, "3b66d2fc3126607aea63496bf20711163754b1c66b17eeb26afcedbeb6515636") -- Depot 1846388
+addappid(1846389, 1, "2bf2413e244f9a2b2bde534655aa180df7b457cab71887a57a9b0deccad35c85") -- Depot 1846389
+addappid(1958732, 1, "25843df9b7f819d813f37153c84d225b2e5abf160136bcc71d492e347a39b898") -- Depot 1958732
+addappid(1958733, 1, "15dcf1493ded032c01d52785745f430d5a923882138aeeea418c5d627a61dff3") -- Depot 1958733
+addappid(1958734, 1, "406ca91c568ea5725d8a35412fc5b52136b0a9609a872085871e12b34955638a") -- Depot 1958734
+addappid(1958735, 1, "811995bbee5052f10090e7559eb4d29d6661e9bbb6aaa1da6aaca601a2abf5a9") -- Depot 1958735
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1958731) -- Need for Speed Unbound Palace Upgrade
+addappid(1982930) -- EA Play Trial Key
+addappid(2055280) -- NFS 22 - Standard Pre-Order Key
+addappid(2057870) -- NFS 22 - Palace Edition Pre-Order Key
+addappid(2057880) -- NFS 22 - Palace Edition Key
+addappid(2075010) -- NFS 22 - Standard Edition Key
+addappid(2298540) -- Need for Speed Unbound - Keys to the Map
+addappid(2339280) -- Need for Speed Unbound - Vol.3 Customs Pack
+addappid(2339281) -- Need for Speed Unbound - Robojets Swag Pack
+addappid(2339282) -- Need for Speed Unbound - Ford Mustang GT Legendary Custom Pack
+addappid(2339283) -- Need for Speed Unbound - Vol.4 Customs Pack
+addappid(2339284) -- Need for Speed Unbound - Hip Hop Origin Swag Pack
+addappid(2339285) -- Need for Speed Unbound - Lotus Exige S Legendary Custom Pack
+addappid(2339286) -- Need for Speed Unbound - Vol.5 Customs Pack
+addappid(2339287) -- Need for Speed Unbound  Trick or Street Swag Pack
+addappid(2339288) -- Need for Speed Unbound - Volkswagen Beetle (1963) Legendary Custom Pack
+addappid(2566530) -- Need for Speed Unbound - Vol.3 Catch-Up Pack
+addappid(2566540) -- Need for Speed Unbound - Vol.4 Catch-Up Pack
+addappid(2749280) -- Need for Speed Unbound - Vol.6 Premium Speed Pass
+addappid(2749290) -- Need for Speed Unbound - Vol.5 Catch-Up Pack
+addappid(2856150) -- Need for Speed Unbound - Vol.7 Premium Speed Pass
+addappid(2856160) -- NEED FOR SPEED UNBOUND - Volume 6 Dynamic Catchup Pack
+addappid(2856170) -- Need for Speed Unbound - Vol.8 Premium Speed Pass
+addappid(2856180) -- NEED FOR SPEED UNBOUND - Volume 7 Dynamic Catchup Pack
+addappid(2856190) -- Need for Speed Unbound  Vol. 9 Premium Speed Pass
+addappid(2856200) -- Need for Speed Unbound  Vol. 8 Dynamic Catch-Up Pack
+addappid(2926920) -- Need for Speed Unbound - Volume 6 Car Pack
+addappid(2926930) -- Need for Speed Unbound - Ford Mustang Dark Horse 24 Set
+addappid(2926940) -- Need for Speed Unbound  Porsche 959 S 87 Set
+addappid(3145990) -- Need for Speed Unbound Complete Edition Key
+addappid(3156380) -- Need for Speed Unbound Ultimate Collection Upgrade
