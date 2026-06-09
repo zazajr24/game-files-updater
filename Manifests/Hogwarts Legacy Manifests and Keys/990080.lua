4d6b1c3353e486cef47d57cf543d2a62ed083ea2
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:40:08 +0200

    Update 990080.lua

---
+++ Manifests/Hogwarts Legacy Manifests and Keys/990080.lua
@@ -0,0 +1,30 @@
+-- 990080's Lua and Manifest Created by Hubcap Manifest
+-- Hogwarts Legacy
+-- Created: December 03, 2025 at 12:13:20 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 13
+-- Total DLCs: 4
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(990080, 1, "da8234fa54a6758153c90b4d013b5a5e41930ef8a22c1e42729949bb890e0c55") -- Hogwarts Legacy
+-- MAIN APP DEPOTS
+addappid(990081, 1, "99edd7f3221635d93481770b28ebac240488e1f42cf16f01e5a4e744b0944da6") -- Depot 990081
+addappid(990082, 1, "ac55581fbdef1f6687f91654d435e2d4c395d2fa56eb5aba4fa52d708118157e") -- Depot 990082
+addappid(990083, 1, "2326f907ccd6ebfc8235495fbb2abd7b12d814a3228cd3b0bf480f7c602c87f5") -- Depot 990083
+addappid(990084, 1, "f043f2ca0cfbbaf5fcb6e30d4beb1a20ba5ef293db11dc4e5521bdfa8a7fdfe0") -- Depot 990084
+addappid(990085, 1, "b7d07ff314786df30f9d32266a2e88cd026fabaffbd2567dbc1637cd319ef996") -- Depot 990085
+addappid(990086, 1, "d9814846faaa5f83e45151bec5190062e320c6b9a6842e7722ed51293a29a667") -- Depot 990086
+addappid(990087, 1, "42c46e0e1520cefc9f4120f00fca640bc83f48b322e7b19ad9530538d9cbee75") -- Depot 990087
+addappid(990088, 1, "81c364d84279c2fafb7960d0aabd75e7e371ad27f275484d99ef5734e4c00d95") -- Depot 990088
+addappid(990089, 1, "21886ab063a78ccf9b676f311597b895c73a1410cd9f7e0e3e7b4e99b82f1e6b") -- Depot 990089
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1880830) -- Hogwarts Legacy Onyx Hippogriff Mount
+addappid(1880832) -- Hogwarts Legacy Dark Arts Pack
+addappid(1880833) -- Hogwarts Legacy Kelpie Robe
+addappid(1880837) --  Hogwarts Legacy Dark Arts Garrison Hat
