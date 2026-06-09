b7b1064548fabf062a538cecbb179cc9df324a6f
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:43:31 +0200

    Update 2513280.lua

---
+++ Manifests/SONIC X SHADOW GENERATIONS Manifests and Keys/2513280.lua
@@ -0,0 +1,35 @@
+-- 2513280's Lua and Manifest Created by Hubcap Manifest
+-- SONIC X SHADOW GENERATIONS
+-- Created: September 30, 2025 at 18:22:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 8
+-- Total DLCs: 5
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2513280) -- SONIC X SHADOW GENERATIONS
+addtoken(2513280, "14381635061238922710")
+-- MAIN APP DEPOTS
+addappid(2513281, 1, "e5b9cddfe91ed04af8c6d17537301797ab16a5041a7edca1d45ae281208a740f") -- Depot 2513281
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- SONIC X SHADOW GENERATIONS Extra Content Pack (AppID: 2909770)
+addappid(2909770)
+addtoken(2909770, "5080527727914125587")
+addappid(2909770, 1, "45c41ebbd5561b69146ab5f7ecc497f31ee3e82db476188d8af2ae31537ae58a") -- SONIC X SHADOW GENERATIONS Extra Content Pack - Depot 2909770
+-- SONIC X SHADOW GENERATIONS Sonic Adventure Legacy Skin (AppID: 2909780)
+addappid(2909780)
+addtoken(2909780, "13118782285101610559")
+addappid(2909780, 1, "d4bca264bdbcdde86f3167d63185b55ea30748dc1670b75bd2c48fe00b16ec08") -- SONIC X SHADOW GENERATIONS Sonic Adventure Legacy Skin - Depot 2909780
+-- SONIC X SHADOW GENERATIONS Sonic Jam Legacy Skin (AppID: 2909790)
+addappid(2909790)
+addappid(2909790, 1, "bf6e60e47f8307ecf6fa9f9d935d223475b099b56d9da116e8e52377b5b971b4") -- SONIC X SHADOW GENERATIONS Sonic Jam Legacy Skin - Depot 2909790
+-- SONIC X SHADOW GENERATIONS Sonic the Hedgehog 3 Movie Pack (AppID: 2983600)
+addappid(2983600)
+addappid(2983600, 1, "bc2088d641e09b580bd27cc38e66095166613065bbe61e6bd0af493143ed106c") -- SONIC X SHADOW GENERATIONS Sonic the Hedgehog 3 Movie Pack - Depot 2983600
+-- SONIC X SHADOW GENERATIONS Art Book with Mini Soundtrack (AppID: 2999160)
+addappid(2999160)
+addtoken(2999160, "7926112299363815962")
+addappid(2999160, 1, "5333f0019f11df5032e635e73031fddfb9d2b9a631779e9d4cf309b15397cf7a") -- SONIC X SHADOW GENERATIONS Art Book with Mini Soundtrack - Depot 2999160
