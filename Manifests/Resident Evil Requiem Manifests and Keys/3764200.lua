f51a8bd56a7d288219fea9eadb5fe4ad61a0a184
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:43:18 +0200

    Update 3764200.lua

---
+++ Manifests/Resident Evil Requiem Manifests and Keys/3764200.lua
@@ -0,0 +1,24 @@
+-- 3764200's Lua and Manifest Created by Hubcap Manifest
+-- Resident Evil Requiem
+-- Created: May 07, 2026 at 21:28:06 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 2
+-- Shared Depots: 1
+-- Blacklisted Depots: 4
+--   Depot 3764202, 3764204, 3764205, 3764206: Blacklisted
+
+-- MAIN APPLICATION
+addappid(3764200, 1, "d29b7ad461b8b74dce38b7137fc00d4b5a885c5be1211767a4ecae0eaf8ddd96") -- Resident Evil Requiem
+-- MAIN APP DEPOTS
+addappid(3764201, 1, "bdf443ffde6449192b9863c0fa5e3cda31fdaa5e4d33e5bcded62dc2085b7cb8") -- Depot 3764201
+addappid(3764203, 1, "b2e820287e6e3f32d1ccb3f2f8c95ec3783e8c0a75abab5d311a0a648626fd92") -- Depot 3764203
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Resident Evil Requiem - Graces Costume Apocalypse (AppID: 3990800)
+addappid(3990800)
+addappid(3990800, 1, "f5bf116706491176c9eee46a96fe1faa70b3f771d75ccb34f0c8962097a907e5") -- Resident Evil Requiem - Graces Costume Apocalypse - Depot 3990800
+-- Resident Evil Requiem - Deluxe Kit (AppID: 3990820)
+addappid(3990820)
+addappid(3990820, 1, "9ae4713cb4114effdd34d1cd54dbda8a560f27a05aef06fbaa0370748f69d15c") -- Resident Evil Requiem - Deluxe Kit - Depot 3990820
