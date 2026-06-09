e5ac2c1ed31312b64c426fa75161c3b92d4e167c
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:41:14 +0200

    Update 2688950.lua

---
+++ Manifests/Planet Coaster 2 Manifests and Keys/2688950.lua
@@ -0,0 +1,21 @@
+-- 2688950's Lua and Manifest Created by Hubcap Manifest
+-- Planet Coaster 2
+-- Created: May 18, 2026 at 10:42:09 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 5
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2688950, 1, "db80e514e0e23f3ac57084d4e47af67ec4d0d5d3c223d5b918daf8fdc9e73eeb") -- Planet Coaster 2
+-- MAIN APP DEPOTS
+addappid(2688951, 1, "d1fc4044f8dbec56ccd7ccac0a38810470c5fecf00dedc36a294c39453f9adb0") -- Depot 2688951
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3091180) -- Planet Coaster 2 Bonus Ride Collection
+addappid(3091190) -- Planet Coaster 2 Vintage Funfair Ride Pack
+addappid(3337040) -- Planet Coaster 2 Thrill-Seekers Ride Pack
+addappid(3902620) -- Planet Coaster 2 Sorcery Pack
+addappid(4129920) -- Planet Coaster 2 Toybox Pack
