f401bb435b99ef0200d817183bffdf284b80b233
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:55:23 +0200

    Update 2072450.lua

---
+++ Manifests/Like a Dragon_ Infinite Wealth Manifests and Keys/2072450.lua
@@ -0,0 +1,35 @@
+-- 2072450's Lua and Manifest Created by Hubcap Manifest
+-- Like a Dragon: Infinite Wealth
+-- Created: September 29, 2025 at 22:11:40 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 20
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2072450) -- Like a Dragon: Infinite Wealth
+-- MAIN APP DEPOTS
+addappid(2072451, 1, "77360feef0acf98c8a6727d3cca6f0bd7ede2c3a36fa467945ff217a7067e002") -- Depot 2072451
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2562820) -- Like a Dragon Infinite Wealth - Master Vacation Bundle
+addappid(2562830) -- Like a Dragon Infinite Wealth - Assorted Outfit Bundle
+addappid(2562840) -- Like a Dragon Infinite Wealth - Yakuza CD Collection Set
+addappid(2562850) -- Like a Dragon Infinite Wealth - Sujimon  Resort Bundle
+addappid(2562860) -- Like a Dragon Infinite Wealth - Special Job Set
+addappid(2584300) -- Like a Dragon Infinite Wealth - Heros Booster Pack
+addappid(2606390) -- Like a Dragon Infinite Wealth - Gearworks Crafting Set (Medium)
+addappid(2606400) -- Like a Dragon Infinite Wealth - Gearworks Crafting Set (Large)
+addappid(2606410) -- Like a Dragon Infinite Wealth - Self-Improvement Booster Set (Small)
+addappid(2606420) -- Like a Dragon Infinite Wealth - Self-Improvement Booster Set (Medium)
+addappid(2606430) -- Like a Dragon Infinite Wealth - Self-Improvement Booster Set (Large)
+addappid(2606440) -- Like a Dragon Infinite Wealth - Job Leveling Set (Small)
+addappid(2606450) -- Like a Dragon Infinite Wealth - Job Leveling Set (Medium)
+addappid(2606460) -- Like a Dragon Infinite Wealth - Job Leveling Set (Large)
+addappid(2606470) -- Like a Dragon Infinite Wealth - Leveling Set (Small)
+addappid(2606480) -- Like a Dragon Infinite Wealth - Leveling Set (Medium)
+addappid(2606490) -- Like a Dragon Infinite Wealth - Leveling Set (Large)
+addappid(2606500) -- Like a Dragon Infinite Wealth - Leveling Set (Extra Large)
+addappid(2606510) -- Like a Dragon Infinite Wealth - Legendary Booster Pack
+addappid(2669420) -- Like a Dragon Infinite Wealth - Special Outfit Hello Work Employee (Ichiban)
