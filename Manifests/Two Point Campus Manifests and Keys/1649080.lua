6a28b7c3a1fcd74de5cbfc5f488075c76a37cacd
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:42:10 +0200

    Update 1649080.lua

---
+++ Manifests/Two Point Campus Manifests and Keys/1649080.lua
@@ -0,0 +1,37 @@
+-- 1649080's Lua and Manifest Created by Hubcap Manifest
+-- Two Point Campus
+-- Created: December 01, 2025 at 05:59:23 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 16
+-- Total DLCs: 4
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1649080) -- Two Point Campus
+-- MAIN APP DEPOTS
+addappid(1649081, 1, "1ddc414e293d545608445a90c6fd82c425015af67322cc873edabcaa1f55fef3") -- Depot 1649081
+addappid(1649082, 1, "6c213bf56b989f4ba6d1bcae8ba254e950642b294e7071f949008050ca3fa295") -- Depot 1649082
+addappid(1649083, 1, "2dbbf78521011a34e5e5fe5212f5c7bf7c2e45e73ddfc2d278e98d551f4d10e2") -- Depot 1649083
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Two Point Campus Space Academy (AppID: 1884560)
+addappid(1884560)
+addappid(1884561, 1, "3ab8ff3b320cd65331dd35e43a55f3413929d16203241ba718f6c0439a07317c") -- Two Point Campus Space Academy - Depot 1884561
+addappid(1884562, 1, "0a01598b0351ff1d854ebc0f487c539eb3a1590b02591ff4c7bd33529cc4bb16") -- Two Point Campus Space Academy - Depot 1884562
+addappid(1884563, 1, "d610269928a2876f1b0bff08ec9134b50799e2aed5c8b08a9eb6b09b9ae1df1a") -- Two Point Campus Space Academy - Depot 1884563
+-- Two Point Campus School Spirits (AppID: 1907450)
+addappid(1907450)
+addappid(1907451, 1, "6ea6777bc5bf9ebac8a275eee91ab493039f0260e02bf0171e2ad5a28c325330") -- Two Point Campus School Spirits - Depot 1907451
+addappid(1907452, 1, "8c993f289dcafecd1a6eb12ae6b898aa393be804f5dd776573a135feaa38670b") -- Two Point Campus School Spirits - Depot 1907452
+addappid(1907453, 1, "c7a282a91834d06f54e2ee9d742a1c9daf7f2068db97b44af510fe7b435ec568") -- Two Point Campus School Spirits - Depot 1907453
+-- Two Point Campus Early Adopter Bonus (AppID: 2088830)
+addappid(2088830)
+addappid(2088831, 1, "3e6d3b66088fbf4a1729e2408b8d4aee76c1146c6089106feb2b33b8d3f6537e") -- Two Point Campus Early Adopter Bonus - Depot 2088831
+addappid(2088832, 1, "e19097e5001abde6bc06410e3ca8b6d6bcf4b1ab522d3a4143c4be4d1fc85548") -- Two Point Campus Early Adopter Bonus - Depot 2088832
+addappid(2088833, 1, "ce49ae5527be38294358e4a55da418ed0d1a7191913fba8b53344a9f79d7a1c5") -- Two Point Campus Early Adopter Bonus - Depot 2088833
+-- Two Point Campus Medical School (AppID: 2195430)
+addappid(2195430)
+addappid(2195431, 1, "bf0c668095ff55b5d460ef76ece6edc9c66d8522cee8aa627384fc269fd9abb3") -- Two Point Campus Medical School - Depot 2195431
+addappid(2195432, 1, "63e8581bc489f09e71fe192255c2179c8b5f26553a3e2020b07e18644a215628") -- Two Point Campus Medical School - Depot 2195432
+addappid(2195433, 1, "7fe91e0d6d9bea5a4093d8eda285d37d93b38ff84f411b8380534a6cb0bed93e") -- Two Point Campus Medical School - Depot 2195433
