0b06e4f8fa24a1b6bb667bbf917ec0a8f503e367
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:40:33 +0200

    Update 2246340.lua

---
+++ Manifests/Monster Hunter Wilds Manifests and Keys/2246340.lua
@@ -0,0 +1,221 @@
+-- 2246340's Lua and Manifest Created by Hubcap Manifest
+-- Monster Hunter Wilds
+-- Created: March 05, 2026 at 22:15:25 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 191
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2246340, 1, "3376b3ba2a4e55044a3116230ebeafcc862380c70ed5329384fcbbeeca7beaf3") -- Monster Hunter Wilds
+-- MAIN APP DEPOTS
+addappid(2246341, 1, "15f418b98b8ade3be07b095f07a9ffc62ab918cf659a2fd7465fcb41c462f0af") -- Depot 2246341
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Monster Hunter Wilds - High Resolution Texture Pack (AppID: 3308900)
+addappid(3308900)
+addappid(3308900, 1, "86e9ff2249c193631e8e04855d9c6154071b06491dced87279c1c72b1662556d") -- Monster Hunter Wilds - High Resolution Texture Pack - Depot 3308900
+-- Monster Hunter Wilds Original Soundtrack (AppID: 3885660)
+addappid(3885660)
+addappid(3885661, 1, "25073952ac95672044fee8fda7b6157f0f00d418f666e53243ace246cd9b0850") -- Monster Hunter Wilds Original Soundtrack - Depot 3885661
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3116990) -- Monster Hunter Wilds - Gesture Set Vol. 1
+addappid(3117090) -- Monster Hunter Wilds - Hunter Layered Armor Set Feudal Soldier
+addappid(3117100) -- Monster Hunter Wilds - Hunter Layered Armor Fencers Eyepatch
+addappid(3117110) -- Monster Hunter Wilds - Hunter Layered Armor Oni Horns Wig
+addappid(3117120) -- Monster Hunter Wilds - Seikret Decoration Soldiers Caparison
+addappid(3117130) -- Monster Hunter Wilds - Seikret Decoration Generals Caparison
+addappid(3117140) -- Monster Hunter Wilds - Felyne Layered Armor Set Felyne Ashigaru
+addappid(3117170) -- Monster Hunter Wilds - Pendant Avian Wind Chime
+addappid(3117180) -- Monster Hunter Wilds - Hairstyle Heros Topknot
+addappid(3117190) -- Monster Hunter Wilds - Hairstyle Refined Warrior
+addappid(3117200) -- Monster Hunter Wilds - MakeupFace Paint Hunters Kumadori
+addappid(3117210) -- Monster Hunter Wilds - MakeupFace Paint Special Bloom
+addappid(3117220) -- Monster Hunter Wilds - Sticker Set Avis Unit
+addappid(3117230) -- Monster Hunter Wilds - Sticker Set Monsters of the Windward Plains
+addappid(3117250) -- Monster Hunter Wilds - Hunter Layered Armor Set Noblesse
+addappid(3117260) -- Monster Hunter Wilds - Hunter Layered Armor Florescent Circlet
+addappid(3117280) -- Monster Hunter Wilds - Seikret Decoration Purehorn Caparison
+addappid(3117290) -- Monster Hunter Wilds - Seikret Decoration Duskhorn Caparison
+addappid(3117300) -- Monster Hunter Wilds - Pendant White Fairy Light
+addappid(3117310) -- Monster Hunter Wilds - Pendant Red Fairy Light
+addappid(3117320) -- Monster Hunter Wilds - Pendant Green Fairy Light
+addappid(3117330) -- Monster Hunter Wilds - Pendant Blue Fairy Light
+addappid(3117340) -- Monster Hunter Wilds - Pendant Violet Fairy Light
+addappid(3117350) -- Monster Hunter Wilds - Pendant Pink Fairy Light
+addappid(3117370) -- Monster Hunter Wilds - Photo Pose Set Vol. 1
+addappid(3117390) -- Monster Hunter Wilds - Photo Pose Adventure Begins Pose 1  2
+addappid(3117400) -- Monster Hunter Wilds - Photo Pose Adventure Begins Pose 3  4
+addappid(3117430) -- Monster Hunter Wilds - MakeupFace Paint Monoblos Heart
+addappid(3117440) -- Monster Hunter Wilds - Tent Customization Set Vol. 1
+addappid(3117450) -- Monster Hunter Wilds - Camp Gear Set Vol. 1
+addappid(3117460) -- Monster Hunter Wilds - BGM Set Vol. 1
+addappid(3117500) -- Monster Hunter Wilds - Sticker Set Expedition Pals
+addappid(3117510) -- Monster Hunter Wilds - Alma Outfit Scriveners Coat
+addappid(3117520) -- Monster Hunter Wilds - Alma Outfit New World Commission
+addappid(3117530) -- Monster Hunter Wilds - Photo Pose Set Vol. 2
+addappid(3117540) -- Monster Hunter Wilds - Gesture Celebratory Knee Slide
+addappid(3117550) -- Monster Hunter Wilds - Gesture Rearing To Go
+addappid(3117560) -- Monster Hunter Wilds - Gesture Set Vol. 2
+addappid(3157720) -- Monster Hunter Wilds - Gesture Battle Cry
+addappid(3157730) -- Monster Hunter Wilds - Gesture Uchiko
+addappid(3289620) -- Monster Hunter Wilds - Character Edit Voucher Three-Voucher Pack
+addappid(3289630) -- Monster Hunter Wilds - Character Edit Voucher Single Voucher (Free Trial Version)
+addappid(3289640) -- Monster Hunter Wilds - Alma Outfit Spring Blossom Kimono
+addappid(3289650) -- Monster Hunter Wilds - Seikret Decoration Majestic Dragons Caparison
+addappid(3289660) -- Monster Hunter Wilds - Pendant Sakura Posy White
+addappid(3289670) -- Monster Hunter Wilds - Pendant Sakura Posy Colorful
+addappid(3289680) -- Monster Hunter Wilds - Pendant White Cat Bell
+addappid(3289690) -- Monster Hunter Wilds - Pendant Black Cat Bell
+addappid(3289700) -- Monster Hunter Wilds - Gesture Set Vol. 3
+addappid(3289710) -- Monster Hunter Wilds - Sticker Set Blossomdance
+addappid(3289720) -- Monster Hunter Wilds - Hunter Layered Armor Set Cypurrpunk
+addappid(3289730) -- Monster Hunter Wilds - Hairstyle Wild Braids
+addappid(3289740) -- Monster Hunter Wilds - Hairstyle Futuristic Crop
+addappid(3289750) -- Monster Hunter Wilds - MakeupFace Paint Drawn Eyelashes
+addappid(3289760) -- Monster Hunter Wilds - MakeupFace Paint Flames
+addappid(3289770) -- Monster Hunter Wilds - Pendant Strigible Amstrigian
+addappid(3289780) -- Monster Hunter Wilds - Pendant Strigible Hymstrigian
+addappid(3289790) -- Monster Hunter Wilds - Pendant Strigible Cypurrpunk
+addappid(3289800) -- Monster Hunter Wilds - Pendant Strigible Gold
+addappid(3289810) -- Monster Hunter Wilds - Pendant Strigible Love
+addappid(3289820) -- Monster Hunter Wilds - Pendant Strigible Color
+addappid(3289830) -- Monster Hunter Wilds - Gesture Set Vol. 4
+addappid(3289840) -- Monster Hunter Wilds - Gesture Set Vol. 5
+addappid(3289850) -- Monster Hunter Wilds - Sticker Set Whimsical Wudwuds
+addappid(3289860) -- Monster Hunter Wilds - Hairstyle Gemma
+addappid(3289870) -- Monster Hunter Wilds - Hairstyle Alma
+addappid(3289880) -- Monster Hunter Wilds - Hairstyle Nata
+addappid(3289890) -- Monster Hunter Wilds - Gesture Set Vol. 6
+addappid(3289900) -- Monster Hunter Wilds - Alma Outfit Summer Poncho
+addappid(3289910) -- Monster Hunter Wilds - Seikret Decoration Phoenix Caparison
+addappid(3289920) -- Monster Hunter Wilds - Pendant Red Flamefete Spirit
+addappid(3289930) -- Monster Hunter Wilds - Pendant Blue Flamefete Spirit
+addappid(3289940) -- Monster Hunter Wilds - Pendant Violet Flamefete Spirit
+addappid(3289950) -- Monster Hunter Wilds - Pendant Green Flamefete Spirit
+addappid(3289960) -- Monster Hunter Wilds - Gesture Set Vol. 7
+addappid(3289980) -- Monster Hunter Wilds - Sticker Set Flamefete
+addappid(3336850) -- Monster Hunter Wilds - Palico Edit Voucher Three-Voucher Pack
+addappid(3336860) -- Monster Hunter Wilds - Palico Edit Voucher Single Voucher (Free Trial Version)
+addappid(3336890) -- Monster Hunter Wilds - Character  Palico Edit Voucher Three-Voucher Pack
+addappid(3336910) -- Monster Hunter Wilds - Pendant Felyne Mint Chocolate
+addappid(3336920) -- Monster Hunter Wilds - Pendant Felyne Strawberry
+addappid(3336930) -- Monster Hunter Wilds - Pendant Felyne Caramel
+addappid(3360450) -- Monster Hunter Wilds - Pendant Dancing White Cat
+addappid(3360460) -- Monster Hunter Wilds - Pendant Stretched White Cat
+addappid(3360470) -- Monster Hunter Wilds - Pendant Dancing Black Cat
+addappid(3360480) -- Monster Hunter Wilds - Pendant Stretched Black Cat
+addappid(3360490) -- Monster Hunter Wilds - Alma Outfit Chun-Li Outfit - SF6
+addappid(3360500) -- Monster Hunter Wilds - Alma Outfit Cammy Outfit - SF6
+addappid(3360510) -- Monster Hunter Wilds - Gesture SF6 Hadoken
+addappid(3360520) -- Monster Hunter Wilds - Gesture SF6 Shoryuken
+addappid(3360530) -- Monster Hunter Wilds - Sticker Set Street Fighter 6 Set
+addappid(3360540) -- Monster Hunter Wilds - Gemma Outfit Summer Coveralls
+addappid(3360550) -- Monster Hunter Wilds - Erik Outfit Summer Hat
+addappid(3410840) -- Monster Hunter Wilds - Hunter Help Welcome Pack
+addappid(3410850) -- Monster Hunter Wilds - Pendant Blanka-Chan Doll
+addappid(3502660) -- Monster Hunter Wilds - Gesture SF6 Tatsumaki Senpu-kyaku
+addappid(3619120) -- Monster Hunter Wilds - Pendant Fluttering Quatreflies
+addappid(3619130) -- Monster Hunter Wilds - Pendant Trot-along Crake
+addappid(3619140) -- Monster Hunter Wilds - Pendant Floating Hearts
+addappid(3619150) -- Monster Hunter Wilds - Pendant Floating Doodles
+addappid(3619160) -- Monster Hunter Wilds - Pendant Floating Sparkles
+addappid(3619170) -- Monster Hunter Wilds - Pendant Floating Potions
+addappid(3619180) -- Monster Hunter Wilds - Pendant Floating Well-done Steaks
+addappid(3619190) -- Monster Hunter Wilds - Pendant Floating Honey
+addappid(3619200) -- Monster Hunter Wilds - Pendant Pink Flamefete Spirit
+addappid(3619210) -- Monster Hunter Wilds - Pendant White Flamefete Spirit
+addappid(3619220) -- Monster Hunter Wilds - Pendant Lagiacrus Plushie
+addappid(3696220) -- Monster Hunter Wilds - Hunter Layered Armor Fluffy Ears
+addappid(3696230) -- Monster Hunter Wilds - Hunter Layered Armor Fluffy Tail
+addappid(3696240) -- Monster Hunter Wilds - Seikret Decoration Cute Critter Caparison
+addappid(3696250) -- Monster Hunter Wilds - Quest Completion Animation Handler Set
+addappid(3696260) -- Monster Hunter Wilds - Quest Completion Animation Smithy Set
+addappid(3696270) -- Monster Hunter Wilds - Felyne Layered Armor Set Felyne Gawkie
+addappid(3696280) -- Monster Hunter Wilds - Pendant Red Lala Dress Doll
+addappid(3696290) -- Monster Hunter Wilds - Pendant Blue Lala Dress Doll
+addappid(3696300) -- Monster Hunter Wilds - Pendant Pink Lala Dress Doll
+addappid(3696310) -- Monster Hunter Wilds - Tent Customization Set Vol. 2
+addappid(3696340) -- Monster Hunter Wilds - Camp Gear Set Vol. 2
+addappid(3696350) -- Monster Hunter Wilds - BGM Set Vol. 2
+addappid(3696360) -- Monster Hunter Wilds - Sticker Set Endearing Endemic Life
+addappid(3696370) -- Monster Hunter Wilds - Alma Outfit Autumn Witch
+addappid(3696380) -- Monster Hunter Wilds - Erik Outfit Autumn Therian
+addappid(3696390) -- Monster Hunter Wilds - Seikret Decoration Spooky Doll Caparison
+addappid(3696400) -- Monster Hunter Wilds - Gesture Hocus Palicosus
+addappid(3696420) -- Monster Hunter Wilds - Gesture Flying Broomstick
+addappid(3696430) -- Monster Hunter Wilds - Pendant White Poltercat
+addappid(3696440) -- Monster Hunter Wilds - Pendant Orange Poltercat
+addappid(3696450) -- Monster Hunter Wilds - Pendant Violet Poltercat
+addappid(3696460) -- Monster Hunter Wilds - Pendant Blue Poltercat
+addappid(3696470) -- Monster Hunter Wilds - Sticker Set Pillopod Pathos
+addappid(3696480) -- Monster Hunter Wilds - Camp Gear Set Vol. 3
+addappid(3696610) -- Monster Hunter Wilds - Hunter Layered Armor Set Cosmoloid
+addappid(3696620) -- Monster Hunter Wilds - Seikret Decoration Machina Caparison
+addappid(3696630) -- Monster Hunter Wilds - Layered Sword  Shield Cosmoloid Saber
+addappid(3696640) -- Monster Hunter Wilds - Layered Great Sword Cosmoloid Blade
+addappid(3696750) -- Monster Hunter Wilds - Layered Long Sword Cosmoloid Edge
+addappid(3696760) -- Monster Hunter Wilds - Layered Dual Blades Cosmoloid Wings
+addappid(3696770) -- Monster Hunter Wilds - Layered Lance Cosmoloid Piercer
+addappid(3696780) -- Monster Hunter Wilds - Layered Gunlance Cosmoloid Burst
+addappid(3696790) -- Monster Hunter Wilds - Layered Switch Axe Cosmoloid Axe
+addappid(3696800) -- Monster Hunter Wilds - Layered Charge Blade Cosmoloid Arms
+addappid(3696810) -- Monster Hunter Wilds - Layered Hammer Cosmoloid Collapse
+addappid(3696820) -- Monster Hunter Wilds - Layered Insect Glaive Cosmoloid Glaive
+addappid(3696830) -- Monster Hunter Wilds - Layered Hunting Horn Cosmoloid Gjallar
+addappid(3696840) -- Monster Hunter Wilds - Layered Light Bowgun Cosmoloid Shooter
+addappid(3696850) -- Monster Hunter Wilds - Layered Heavy Bowgun Cosmoloid Buster
+addappid(3696860) -- Monster Hunter Wilds - Layered Bow Cosmoloid Arrow
+addappid(3696870) -- Monster Hunter Wilds - Pendant Cosmoloid Core
+addappid(3696890) -- Monster Hunter Wilds - Photo Pose Seikret Battle-Ready Pose
+addappid(3696900) -- Monster Hunter Wilds - Sticker Set Everyday Eastlands
+addappid(3696910) -- Monster Hunter Wilds - Hairstyle Olivia
+addappid(3696920) -- Monster Hunter Wilds - Hairstyle Erik
+addappid(3696930) -- Monster Hunter Wilds - Hairstyle Werner
+addappid(3696940) -- Monster Hunter Wilds - Pendant White Rock-a-Baby
+addappid(3696950) -- Monster Hunter Wilds - Pendant Pink Rock-a-Baby
+addappid(3696960) -- Monster Hunter Wilds - Pendant Blue Rock-a-Baby
+addappid(3696970) -- Monster Hunter Wilds - Pendant Green Rock-a-Baby
+addappid(3696980) -- Monster Hunter Wilds - Alma Outfit Featherskirt Seikret Dress
+addappid(3696990) -- Monster Hunter Wilds - Erik Outfit Crestcollar Seikret Suit
+addappid(3697010) -- Monster Hunter Wilds - Gemma Outfit Redveil Seikret Dress
+addappid(3697020) -- Monster Hunter Wilds - Seikret Decoration Whitewing Caparison
+addappid(3697030) -- Monster Hunter Wilds - Gesture Set Vol. 9
+addappid(3697050) -- Monster Hunter Wilds - Sticker Set Avis Unit Greetings
+addappid(3747820) -- Monster Hunter Wilds - Pendant Felyne Duna Bobble
+addappid(3747830) -- Monster Hunter Wilds - Pendant Felyne Duna  Bobble
+addappid(3747840) -- Monster Hunter Wilds - Pendant Fluttering Myriads
+addappid(3747850) -- Monster Hunter Wilds - Gesture Resting Seikret
+addappid(3747860) -- Monster Hunter Wilds - Gesture Tea Time
+addappid(3767510) -- Monster Hunter Wilds - Pendant Orange Lala Dress Doll
+addappid(3767520) -- Monster Hunter Wilds - Pendant Violet Lala Dress Doll
+addappid(3767530) -- Monster Hunter Wilds - Pendant Green Poltercat
+addappid(3772970) -- Monster Hunter Wilds - Pendant Red Poltercat
+addappid(3801040) -- Monster Hunter Wilds - Gesture Set Vol. 8
+addappid(3932400) -- Monster Hunter Wilds - Pendant Trot-along Fledgling
+addappid(3932410) -- Monster Hunter Wilds - Pendant White Turntail
+addappid(3932420) -- Monster Hunter Wilds - Pendant White Tubby Turntail
+addappid(3932430) -- Monster Hunter Wilds - Pendant Black Turntail
+addappid(3932440) -- Monster Hunter Wilds - Pendant Black Tubby Turntail
+addappid(3932450) -- Monster Hunter Wilds - Pendant Floating Rath Balloon
+addappid(3932460) -- Monster Hunter Wilds - Gesture Set Vol. 10
+addappid(3932490) -- Monster Hunter Wilds - Pendant Purple Rock-a-Baby
+addappid(3932500) -- Monster Hunter Wilds - Pendant Black Rock-a-Baby
+addappid(3959310) -- Monster Hunter Wilds - Pendant F Hope Balloon
+addappid(3959320) -- Monster Hunter Wilds - Pendant F Arkveld Balloon
+addappid(3959330) -- Monster Hunter Wilds - Pendant F Rompopolo Balloon
+addappid(3959340) -- Monster Hunter Wilds - Pendant F Barina Balloon
+addappid(3959350) -- Monster Hunter Wilds - Pendant Athos Balloon
+addappid(3968920) -- Monster Hunter Wilds - Character  Palico Edit Voucher Single Voucher (Free Gift Version)
+addappid(3968930) -- Monster Hunter Wilds - Character  Palico Edit Voucher Single Voucher (Free Gift Version 2)
+-- FORCE-ADDED DLCS (BYPASSES ALL VALIDATION)
+addappid(3116950) -- Monster Hunter Wilds - Hunter Layered Armor Set: Guild Knight
+addappid(3116960) -- Monster Hunter Wilds - Talisman: Hope Charm
+addappid(3117030) -- Monster Hunter Wilds - Hunter Layered Armor: Wyverian Ears
+addappid(3117050) -- Monster Hunter Wilds - Premium Bonus Hunter Profile Set
+addappid(3117070) -- Monster Hunter Wilds - BGM: Proof of a Hero (2025 Recording)
+addappid(3117240) -- Monster Hunter Wilds - Nameplate: Extra Frame — Russet Dawn
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3117420) -- Depot 3117420 (empty depot)
