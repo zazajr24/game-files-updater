5300325f7f16e19b382e92b9aea804626add6192
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:41:05 +0200

    Update 1687950.lua

---
+++ Manifests/Persona 5 Royal Manifests and Keys/1687950.lua
@@ -0,0 +1,26 @@
+-- 1687950's Lua and Manifest Created by Hubcap Manifest
+-- Persona 5 Royal
+-- Created: January 20, 2026 at 10:41:43 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 12
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1687950, 1, "213f5a64fac8e551f7588d59a4a32e22feaf3b9118544503f555b3949f5db67f") -- Persona 5 Royal
+-- MAIN APP DEPOTS
+addappid(1687951, 1, "63d197a9660d17f1f93004f842b51a99590dcb140cfa4b33b67f04b66e3ef2dc") -- Depot 1687951
+addappid(1687952, 1, "786ef5979ef545b2322dcda59e5f0ccaaadd321a9f6ac7522b6a55198edff0b6") -- Depot 1687952
+addappid(1687953, 1, "0d1a567ea2ea013eb35cae7c62deac6973987d8f0cf5585326fc9f0cec3f0868") -- Depot 1687953
+addappid(1687954, 1, "26d7d88b05209c16a6ca45297dcaaa774b2185b6a74970bac121c1f5cb37b1d9") -- Depot 1687954
+addappid(1687955, 1, "987640b74d60f9c60cbf8d2899469cd1d552f00df1da5a3aba3dfa44ea6f375a") -- Depot 1687955
+addappid(1687956, 1, "c7b8a0e3c11d039f34b797ce0ce792926c0cb6e70c5cdac969785c85d7d29133") -- Depot 1687956
+addappid(1687957, 1, "cd181cf543f85bc275f52e8836e5090293530f895c6109be61bbf8735fd1fa60") -- Depot 1687957
+addappid(1687958, 1, "bcd88777efb6608bbd36b99c813acfda482f0a0ae1ebbef6844e9f9bb46c5872") -- Depot 1687958
+addappid(1687959, 1, "4ed788043739d211ea18eaf3837f9739527b60da089c778b896d9baa05a16415") -- Depot 1687959
+addappid(1753980, 1, "5373b843f2355df32399e1130a468df8893039961569bde75192c1be9e05f0d6") -- Depot 1753980
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1753981) -- Depot 1753981 (empty depot)
