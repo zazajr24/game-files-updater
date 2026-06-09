f2e435b39cdeb0b8aecbabd2b8d44d1ec4c618de
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:40:21 +0200

    Update 1244460.lua

---
+++ Manifests/Jurassic World Evolution 2 Manifests and Keys/1244460.lua
@@ -0,0 +1,28 @@
+-- 1244460's Lua and Manifest Created by Hubcap Manifest
+-- Jurassic World Evolution 2
+-- Created: September 29, 2025 at 20:19:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 12
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1244460) -- Jurassic World Evolution 2
+-- MAIN APP DEPOTS
+addappid(1244461, 1, "f32fa164f046ff769b0eb4c8ca8c0841866087c40db3edbec4d5f79b299e5fbe") -- Galileo Content
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1336320) -- JWE 2 Pre-order Content
+addappid(1336321) -- Jurassic World Evolution 2 Deluxe Upgrade Pack
+addappid(1336322) -- Jurassic World Evolution 2 Early Cretaceous Pack
+addappid(1882470) -- Jurassic World Evolution 2 Camp Cretaceous Dinosaur Pack
+addappid(1966010) -- Jurassic World Evolution 2 Dominion Biosyn Expansion
+addappid(2106800) -- Jurassic World Evolution 2 Late Cretaceous Pack
+addappid(2177430) -- Jurassic World Evolution 2 Dominion Malta Expansion
+addappid(2314000) -- Jurassic World Evolution 2 Feathered Species Pack
+addappid(2492640) -- Jurassic World Evolution 2 Prehistoric Marine Species Pack
+addappid(2610980) -- Jurassic World Evolution 2 Cretaceous Predator Pack
+addappid(2806860) -- Jurassic World Evolution 2 Secret Species Pack
+addappid(2897800) -- Jurassic World Evolution 2 Park Managers Collection Pack
