dd955b3a8da825993ee6799b2fd2a16863e1005e
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-09 19:45:41 +0200

    Update 2195250.lua

---
+++ Manifests/EA SPORTS FC™ 24 Manifests and Keys/2195250.lua
@@ -0,0 +1,46 @@
+-- 2195250's Lua and Manifest Created by Hubcap Manifest
+-- EA SPORTS FC™ 24
+-- Created: June 04, 2026 at 14:27:34 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 24
+-- Total DLCs: 9
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2195250, 1, "b4ca944ad11b1e1dc05553de12da93e6e9b85af64c0508bfbd8ffa6282d72aa7") -- EA SPORTS FC™ 24
+-- MAIN APP DEPOTS
+addappid(2195251, 1, "78feaa85fe71b42fbdd3ee6c295b53a827cb378055fcb879e1d64f570cab4000") -- Depot 2195251
+addappid(2195252, 1, "6610013ba8d9189db540d940af7e4d7c93df64399fba23b792c309d9e4f6fc45") -- Depot 2195252
+addappid(2195253, 1, "e6d3d48efddcb0263a5b76e1ee4041f0604da0abd7e78e5139f9162d58545c6a") -- Depot 2195253
+addappid(2195254, 1, "fbad61b6b52e42381f852bee04b591373e4266d11b70225b94b9de3d954dad04") -- Depot 2195254
+addappid(2195255, 1, "af62d16f12b3be6be06ca50d8823545088fb164f4507676ab8dafbcc8d292e2d") -- Depot 2195255
+addappid(2195256, 1, "659c2152567039c4d737941e07ec5c8362432d97e0ea77035801beed5d3d3225") -- Depot 2195256
+addappid(2195257, 1, "bac9060f0a96a880bb85dd26b91ef08c7f0b3a4fffbbf1dc460225d216621aa0") -- Depot 2195257
+addappid(2195258, 1, "53c9c9423ab4f2ea70a707bc3714c71fd8fb215573ddef35b801e62725789f15") -- Depot 2195258
+addappid(2195259, 1, "bab489b7b7ff982c2b1b8df353ff355254d62f0f0267f721d4beb14b879a02ad") -- Depot 2195259
+addappid(2195261, 1, "a612da51977ed9d3ff86d752a581b088790737372ee2c2c87d4e42c75370759b") -- Depot 2195261
+addappid(2195262, 1, "930d228afafed0ff7f1e50c9386840b51e4a58a6a3d6d63eb6dd2130c531f46d") -- Depot 2195262
+addappid(2195263, 1, "5eadc028e84890a799f57b8fb811bb250666150f9e2ed62054d58fc1f4bda372") -- Depot 2195263
+addappid(2195264, 1, "2a7dfae8b0f28c00396dcce7ce6baa56db96e9025ca97cff14c39fba27445d08") -- Depot 2195264
+addappid(2195265, 1, "b87c36511a8d1388d8fcd3795eef14d1960c8b768dafc35f44e5c9ca176b131c") -- Depot 2195265
+addappid(2195266, 1, "c1b873816fddabaca855b5ff48655a186e47b22b672a1bc39c80c95c0979a120") -- Depot 2195266
+addappid(2195267, 1, "c50f8b01317ca127737e8818f9102b17bc5e59ff8897f427f93f713add0a039e") -- Depot 2195267
+addappid(2195268, 1, "7fdd0fd8f3657057d53145bd183fe91310346328515ec7d892a3dca4be27cc2f") -- Depot 2195268
+addappid(2195269, 1, "a3eec86fb1c3894a614d27ab29312119dc2688276ff3d3849dcfd492f423f537") -- Depot 2195269
+addappid(2290730, 1, "89604fd2ece112dae44a59061963d8b7a1c40835b55a171ff9cbdec9a051279f") -- Depot 2290730
+addappid(2290731, 1, "f341de9cd8dff0e0fbc8f05eba9f15dd12d92b07dea1fc196b488c801e646e9f") -- Depot 2290731
+addappid(2290732, 1, "b56ace186cab7c48b3fb32401509937d56fca2fac360fc1a99379169c875a1c3") -- Depot 2290732
+addappid(2290733, 1, "91c3f1cf3d4050f5b5e1994810c54c7a49de8ea647d3143df67a58caee51950b") -- Depot 2290733
+-- SHARED DEPOTS (from other apps)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+addappid(3893181, 1, "7f675c2fe8e758d16f3cf8d3b493956afaee78e96d78cdb668a5edb8ac1580f6") -- Depot 3893181 (Shared from App 3893180)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2195260) -- EA Sports FC 24 Standard Edition - Key
+addappid(2308280) -- EA Sports FC 24 - EA Play Trial Key
+addappid(2380510) -- EA Sports FC 24 Standard Edition Pre-Order - Key
+addappid(2380511) -- EA Sports FC 24 Early Ultimate Edition Pre-Order - Key
+addappid(2380512) -- EA Sports FC 24 Ultimate Edition Pre-Order - Key
+addappid(2380513) -- EA Sports FC 24 Ultimate Edition - Key
+addappid(2563700) -- EA Sports FC 24 
+addappid(2565620) -- EA Sports FC 24 Press Offer - Key
+addappid(3102930) -- EA SPORTS FC 24 Football Ultimate Team Player Item
