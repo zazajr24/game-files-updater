275581bc94d8d742a42bdc845c2151b79301b476
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-12 17:45:11 +0200

    Update Undisputed Manifests and Keys/1451190.lua

---
+++ Manifests/Undisputed Manifests and Keys/1451190.lua
@@ -0,0 +1,23 @@
+-- 1451190's Lua and Manifest Created by Hubcap Manifest
+-- Undisputed
+-- Created: December 16, 2025 at 10:31:37 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 8
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1451190) -- Undisputed
+-- MAIN APP DEPOTS
+addappid(1451191, 1, "543e5be37b6ba13500441680b3434302f89112d8f1fae261eec6e6cb245ffd22") -- Depot 1451191
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2900100) -- Undisputed - WBC Pack
+addappid(2946350) -- Undisputed - Roy Jones Jr. 93
+addappid(3102510) -- Undisputed - The Problem Child Pack
+addappid(3525000) -- Undisputed - The Iron and Steel Pack
+addappid(3740110) -- Undisputed - The Mexican Monster Pack
+addappid(3870270) -- Undisputed - The Takeover Pack
+addappid(4030960) -- Undisputed - The Senator Pack
+addappid(4031830) -- Undisputed - The Championship Upgrade Pack
