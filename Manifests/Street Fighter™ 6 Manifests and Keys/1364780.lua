8928fb658a589dabc66cb22aaa6003b13c9639bf
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-12 17:46:09 +0200

    Update Street Fighter™ 6 Manifests and Keys/1364780.lua

---
+++ Manifests/Street Fighter™ 6 Manifests and Keys/1364780.lua
@@ -0,0 +1,33 @@
+-- 1364780's Lua and Manifest Created by Hubcap Manifest
+-- Street Fighter™ 6
+-- Created: May 27, 2026 at 22:47:45 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 10
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1364780, 1, "ab1ae48f25d0e9ba9404def1dae8aac005918547d5d9007b5ed3a39b906e7ebd") -- Street Fighter™ 6
+-- MAIN APP DEPOTS
+addappid(1364781, 1, "cfec39716b5a5cae49b52b5c0e01e6db93356ee3df5a0d87590fd811ab6d06e1") -- Depot 1364781
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Street Fighter 6 Fighting Ground (AppID: 1792750)
+addappid(1792750)
+addappid(1792750, 1, "321dd0bd6e467195d8890c9073d183075de7f8a4faf082155b08ef85bea618fe") -- Street Fighter 6 Fighting Ground - Depot 1792750
+-- Street Fighter 6 World Tour (AppID: 1792751)
+addappid(1792751)
+addappid(1792751, 1, "b47545f3d9f54a63bd3cfe4028c8510ad05aead03842627326793508414ba122") -- Street Fighter 6 World Tour - Depot 1792751
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2224460) -- Street Fighter 6 - Year 1 Character Pass
+addappid(2224461) -- Street Fighter 6 - Year 1 Ultimate Pass
+addappid(2224462) -- Street Fighter 6 - Outfit 1 Color 10 for Chun-Li, Jamie, Manon, Dee Jay, Juri, Ken
+addappid(2825190) -- Street Fighter 6 - Year 2 Character Pass
+addappid(2825200) -- Street Fighter 6 - Year 2 Ultimate Pass
+addappid(3401790) -- Street Fighter 6 - Years 1-2 Fighters Edition Stages  Colors
+addappid(3410460) -- Street Fighter 6 - Year 3 Character Pass
+addappid(3410470) -- Street Fighter 6 - Year 3 Ultimate Pass
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(1364782) -- Depot 1364782 (empty depot)
