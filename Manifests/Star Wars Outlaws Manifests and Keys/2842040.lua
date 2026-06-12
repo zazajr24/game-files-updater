b90be99ee769c72c3f6f0c98d405024be18443b8
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-12 18:06:07 +0200

    Update Star Wars Outlaws Manifests and Keys/2842040.lua

---
+++ Manifests/Star Wars Outlaws Manifests and Keys/2842040.lua
@@ -0,0 +1,50 @@
+-- 2842040's Lua and Manifest Created by Hubcap Manifest
+-- Star Wars Outlaws
+-- Created: June 04, 2026 at 11:38:53 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 11
+-- Total DLCs: 23
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2842040, 1, "3682fe856a5ecf5300cc85aa1b928b94995428d60a24ad1b9be2237e70ef1896") -- Star Wars Outlaws
+-- MAIN APP DEPOTS
+addappid(2842041, 1, "bed1b2aedb9b823e084bde3a15663caf8f50feaadd104bba0c7e474540a11f43") -- Depot 2842041
+addappid(2842042, 1, "110e9180cab6db688b21cc6f16820d8197b333d0c1d88b66da81d43753a9c786") -- Depot 2842042
+addappid(2842043, 1, "2d98d4345bb461b3ee9574d5e4f00cf62e968042031e7d7361b3d15d09282788") -- Depot 2842043
+addappid(2842044, 1, "dbaf8d54cceac51941390dffb5257608db23f202ec6d06d61fae96fed743e9d9") -- Depot 2842044
+addappid(2842045, 1, "845fbe248a74375c3dda44984552af44018332d3b7f355dce4d1c833727e0a42") -- Depot 2842045
+addappid(2842046, 1, "58faa2a3ab11d95ab7d448ae380c4211c92726b9fcf619835d2b3900356506ac") -- Depot 2842046
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+-- DLCS WITH DEDICATED DEPOTS
+-- Star Wars Outlaws - DLC1 (AppID: 3271130)
+addappid(3271130)
+addappid(3271130, 1, "cd4259d93979d9d480227cf581dbb3855b238747fa76988d8c94da591300669e") -- Star Wars Outlaws - DLC1 - Depot 3271130
+-- Star Wars Outlaws - A Pirates Fortune DLC (AppID: 3541390)
+addappid(3541390)
+addappid(3541390, 1, "a2c323e9b71ce1d184500fedc2ddaf67f9f96af0f57324757cb7f9949f71944a") -- Star Wars Outlaws - A Pirates Fortune DLC - Depot 3541390
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3112310) -- Star Wars Outlaws - DLC1 - Ubisoft Activation
+addappid(3261290) -- Star Wars Outlaws - Gold Edition - Ubisoft Activation
+addappid(3261340) -- Star Wars Outlaws - Ultimate Edition - Ubisoft Activation
+addappid(3271140) -- Star Wars Outlaws - Season Pass
+addappid(3271150) -- Star Wars Outlaws - Season Pass - Ubisoft Activation
+addappid(3271160) -- Star Wars Outlaws - Ultimate Pack
+addappid(3271170) -- Star Wars Outlaws - Ultimate Pack - Ubisoft Activation
+addappid(3271180) -- Star Wars Outlaws - Forrest Commando
+addappid(3271190) -- Star Wars Outlaws - Forrest Commando - Ubisoft Activation
+addappid(3271200) -- Star Wars Outlaws - Hunters Legacy
+addappid(3271210) -- Star Wars Outlaws - Hunters Legacy - Ubisoft Activation
+addappid(3271230) -- Star Wars Outlaws - Cartel Ronin
+addappid(3271240) -- Star Wars Outlaws - Cartel Ronin - Ubisoft Activation
+addappid(3271460) -- Star Wars Outlaws - Standard Edition - Ubisoft Activation
+addappid(3394020) -- Star Wars Outlaws - Deluxe Edition - Ubisoft Activation
+addappid(3515960) -- Star Wars Outlaws Demo - Ubisoft Activation
+addappid(3541410) -- Star Wars Outlaws - A Pirates Fortune DLC - Ubisoft Activation
+addappid(3702240) -- Star Wars Outlaws - Naboo Nobility Bundle
+addappid(3702250) -- Star Wars Outlaws - Naboo Nobility Bundle Ubisoft Activation
+addappid(3702260) -- Star Wars Outlaws - Desert Nomad Bundle
+addappid(3702270) -- Star Wars Outlaws - Desert Nomad Bundle Ubisoft Activation
