63c8e5727390ec050ee64da32ce1f8fee69e6cdf
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-13 14:23:06 +0200

    Update 2679460.lua

---
+++ Manifests/Metaphor_ ReFantazio Manifests and Keys/2679460.lua
@@ -0,0 +1,57 @@
+-- 2679460's Lua and Manifest Created by Hubcap Manifest
+-- Metaphor: ReFantazio
+-- Created: January 17, 2026 at 19:13:58 EST
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 15
+-- Total DLCs: 15
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2679460, 1, "0f86ac35b51eb5f7680c68841f5e93b1b9db2d9699840546dab0045b6320d00a") -- Metaphor: ReFantazio
+-- MAIN APP DEPOTS
+addappid(2679461, 1, "0a945c91ddfaaca28d19da03981f783f8eb23a6dcc1121fb8c0936bb17fe0adb") -- Depot 2679461
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Metaphor ReFantazio - Shujin Academy School Uniform (7), Battle BGM  Battle Jingle Set  (AppID: 2924400)
+addappid(2924400)
+addappid(2924400, 1, "9e10f74a58642944210d4ac83c2afe05a39a9260522357676d71f7a173b06377") -- Metaphor ReFantazio - Shujin Academy School Uniform (7), Battle BGM  Battle Jingle Set  - Depot 2924400
+-- Metaphor ReFantazio - Yasogami High School Uniform (7), Battle BGM  Battle Jingle Set (AppID: 2924410)
+addappid(2924410)
+addappid(2924410, 1, "f874d47604752cc8683b1f01d6c0af1ba2e70fe2f0b05bb24c9dda0f216f2f6e") -- Metaphor ReFantazio - Yasogami High School Uniform (7), Battle BGM  Battle Jingle Set - Depot 2924410
+-- Metaphor ReFantazio - Gekkoukan High School Uniform (7), Battle BGM  Battle Jingle Set (AppID: 2924420)
+addappid(2924420)
+addappid(2924420, 1, "8d03e6189c0de6ad5de6aef6856b1e737e34fbbc88de72dc4b65ac0dde275909") -- Metaphor ReFantazio - Gekkoukan High School Uniform (7), Battle BGM  Battle Jingle Set - Depot 2924420
+-- Metaphor ReFantazio - Seven Sisters High School Uniform (7), Battle BGM  Battle Jingle Set (AppID: 2924430)
+addappid(2924430)
+addappid(2924430, 1, "d938b2988775f6ee36386f348d136198b811e977d25792973d3efbf25b5ee23d") -- Metaphor ReFantazio - Seven Sisters High School Uniform (7), Battle BGM  Battle Jingle Set - Depot 2924430
+-- Metaphor ReFantazio - St. Hermelin High School Uniform (7), Battle BGM  Battle Jingle Set (AppID: 2924440)
+addappid(2924440)
+addappid(2924440, 1, "f2c1a9b7a12874361ff385a135a0360c33ba6ed85da22e746c14971e5cafec7a") -- Metaphor ReFantazio - St. Hermelin High School Uniform (7), Battle BGM  Battle Jingle Set - Depot 2924440
+-- Metaphor ReFantazio - Jouin High School Uniform (7), Battle BGM  Battle Jingle Set (AppID: 2924450)
+addappid(2924450)
+addappid(2924450, 1, "465e853c2d357a47955eb08ad98b3937f3107d290fade3a96de4439bf7222200") -- Metaphor ReFantazio - Jouin High School Uniform (7), Battle BGM  Battle Jingle Set - Depot 2924450
+-- Metaphor ReFantazio - Samurai Garb (7), Battle BGM  Battle Jingle Set (AppID: 2924460)
+addappid(2924460)
+addappid(2924460, 1, "73b0d5feef1f30f9d2c4a0f864cb8811a39e5705be3938a6257fbc82395278ba") -- Metaphor ReFantazio - Samurai Garb (7), Battle BGM  Battle Jingle Set - Depot 2924460
+-- Metaphor ReFantazio - Etrian Odyssey Class Costumes (7)  Battle BGM Set (AppID: 2924470)
+addappid(2924470)
+addappid(2924470, 1, "16a09d4bd6677c201ad2b9b6164fd3e363aa0e76ce4e703134893da89548fb97") -- Metaphor ReFantazio - Etrian Odyssey Class Costumes (7)  Battle BGM Set - Depot 2924470
+-- Metaphor ReFantazio - Digital Artbook (AppID: 2999630)
+addappid(2999630)
+addappid(2999630, 1, "b7dd710afb9dcb1e0106c84e2354771188f530aa7da4260f05281dda109f69c3") -- Metaphor ReFantazio - Digital Artbook - Depot 2999630
+-- Metaphor ReFantazio - Digital Soundtrack (AppID: 2999640)
+addappid(2999640)
+addappid(2999640, 1, "52fc013d9924ce3cabc75b710214d182cdf62cae67e14fcaeb8ae77e3b546e56") -- Metaphor ReFantazio - Digital Soundtrack - Depot 2999640
+-- Atlus 35th Digital History Book (AppID: 2999650)
+addappid(2999650)
+addappid(2999650, 1, "a2ac12bbb27f3659bc064fef1f20c10644a81b66411a1a38a76da5d395f52b2f") -- Atlus 35th Digital History Book - Depot 2999650
+-- Atlus 35th Digital All-Time Best Soundtrack (AppID: 2999660)
+addappid(2999660)
+addappid(2999660, 1, "64e42d838da4dc83cc7878389b50de24d9aebe6102a7ed346f3d5f69b830d5b6") -- Atlus 35th Digital All-Time Best Soundtrack - Depot 2999660
+-- Metaphor ReFantazio - Essential Digital Strategy Guide  (AppID: 4128470)
+addappid(4128470)
+addappid(4128470, 1, "cfff213631bde08fc999dedfcf4aee678dc3e51c489e2b3879c11bb9908b07b1") -- Metaphor ReFantazio - Essential Digital Strategy Guide  - Depot 4128470
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2924480) -- Metaphor ReFantazio - Archetype EXP Chest Set
+addappid(2924490) -- Metaphor ReFantazio - Adventurers Journey Pack
