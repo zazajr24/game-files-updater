2bdbe75f4c0e8a9ff6d22bf604bf302877d89a6e
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-13 14:38:54 +0200

    Update 3472040.lua

---
+++ Manifests/NBA 2K26 Manifests and Keys/3472040.lua
@@ -0,0 +1,49 @@
+-- 3472040's Lua and Manifest Created by Hubcap Manifest
+-- NBA 2K26
+-- Created: May 15, 2026 at 10:40:01 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 26
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3472040, 1, "c2067181fc839a93369d2d8df998fca7213274e236d2b2653bb1e3341d7bf336") -- NBA 2K26
+-- MAIN APP DEPOTS
+addappid(3472041, 1, "98720ddedf5fb5a37faf70ca90602a9949e727a7f7b6b9171ecc2764ee2d3b21") -- Depot 3472041
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- High Resolution Textures (AppID: 3825290)
+addappid(3825290)
+addtoken(3825290, "4202908082786512814")
+addappid(3472042, 1, "34db1ecbd759eab0b3e63e7304bc8fe901b1ca351a3d453affb865d97c5e62be") -- High Resolution Textures - Depot 3472042
+addappid(3472043, 1, "54228c22842f9dce94c4087041d2cf97e9f00130877da328bf5c4ebb351c7fe0") -- High Resolution Textures - Depot 3472043
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3657560) -- NBA 2K26 SLAM Edition Bonus Pack Offer
+addappid(3657590) -- NBA 2K26 Pro Pass Season 1
+addappid(3657600) -- NBA 2K26 Hall of Fame Pass Season 1
+addappid(3657610) -- NBA 2K26 MyCAREER Bonus Offer Season 1
+addappid(3657620) -- NBA 2K26 MyTEAM Bonus Offer Season 1
+addappid(3657640) -- NBA 2K26 Pro Pass Season 2
+addappid(3657650) -- NBA 2K26 Hall of Fame Pass Season 2
+addappid(3657660) -- NBA 2K26 MyCAREER Bonus Offer Season 2
+addappid(3657670) -- NBA 2K26 MyTEAM Bonus Offer Season 2
+addappid(3657680) -- NBA 2K26 Pro Pass Season 3
+addappid(3657690) -- NBA 2K26 Hall of Fame Pass Season 3
+addappid(3657700) -- NBA 2K26 MyCAREER Bonus Offer Season 3
+addappid(3657710) -- NBA 2K26 MyTEAM Bonus Offer Season 3
+addappid(3657750) -- NBA 2K26 Pro Pass Season 4
+addappid(3657760) -- NBA 2K26 Hall of Fame Pass Season 4
+addappid(3657770) -- NBA 2K26 MyCAREER Season 4 Bonus Offer
+addappid(3657780) -- NBA 2K26 MyTEAM Season 4 Bonus Offer
+addappid(3657790) -- NBA 2K26 Pro Pass Season 5
+addappid(3657800) -- NBA 2K26 Hall of Fame Pass Season 5
+addappid(3657810) -- MyCareer Bundle Season 5
+addappid(3657820) -- NBA 2K26 MyTEAM Season 5 Bonus Offer
+addappid(3657840) -- NBA 2K26 Pro Pass Season 6
+addappid(3657850) -- NBA 2K26 Hall of Fame Pass Season 6
+addappid(3657860) -- NBA 2K26 MyCAREER Season 6 Bonus Offer
+addappid(3657870) -- NBA 2K26 MyTEAM Season 6 Bonus Offer
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(3472044) -- Depot 3472044 (empty depot)
