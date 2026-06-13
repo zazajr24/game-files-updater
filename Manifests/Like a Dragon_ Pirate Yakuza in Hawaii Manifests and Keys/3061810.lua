050c05e959cd13d40d2e4a961ab82e9f46ba281e
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-13 14:21:57 +0200

    Update 3061810.lua

---
+++ Manifests/Like a Dragon_ Pirate Yakuza in Hawaii Manifests and Keys/3061810.lua
@@ -0,0 +1,22 @@
+-- 3061810's Lua and Manifest Created by Hubcap Manifest
+-- Like a Dragon: Pirate Yakuza in Hawaii
+-- Created: September 29, 2025 at 22:11:52 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 7
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3061810) -- Like a Dragon: Pirate Yakuza in Hawaii
+-- MAIN APP DEPOTS
+addappid(3061811, 1, "7a902c59f558f53204bf21403faa08aa113c0eca94bc03ad642f533ed35fc10a") -- Depot 3061811
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3183940) -- Like a Dragon Pirate Yakuza in Hawaii - Ichiban Pirate Crew Set
+addappid(3184000) -- Like a Dragon Pirate Yakuza in Hawaii - Ichiban Special Outfit Set
+addappid(3184010) -- Like a Dragon Pirate Yakuza in Hawaii - Legendary Outfit Pack
+addappid(3184020) -- Like a Dragon Pirate Yakuza in Hawaii - Extra Karaoke  CD Pack 
+addappid(3184030) -- Like a Dragon Pirate Yakuza in Hawaii - Ship Customization Pack
+addappid(3184040) -- Like a Dragon Pirate Yakuza in Hawaii - Legendary Pirate Crew Pack
+addappid(3184050) -- Like a Dragon Pirate Yakuza in Hawaii - Kazuma Kiryu Special Outfit 
