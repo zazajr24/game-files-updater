4ac0b135d68fea9b7d50f7376961c4b82750a606
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-13 14:18:11 +0200

    Update 2361770.lua

---
+++ Manifests/SHINOBI_ Art of Vengeance Manifests and Keys/2361770.lua
@@ -0,0 +1,23 @@
+-- 2361770's Lua and Manifest Created by Hubcap Manifest
+-- SHINOBI: Art of Vengeance
+-- Created: April 04, 2026 at 05:15:03 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 6
+
+-- MAIN APPLICATION
+addappid(2361770, 1, "7802d8db8a3c67b9d5478e24cde9ba28aebeb37dae8d7aec63b5e43ec19cc451") -- SHINOBI: Art of Vengeance
+-- MAIN APP DEPOTS
+addappid(2361771, 1, "6077dc2b1a88a7ae7a34470113e609a77bfc721374f4b17e20abc2f975ad2d85") -- Depot 2361771
+-- DLCS WITH DEDICATED DEPOTS
+-- SHINOBI Art of Vengeance - SEGA Villains Stage (AppID: 3381240)
+addappid(3381240)
+addappid(3381241, 1, "a8471adc3c0b8d999f3576f04318b08df5a03609a11bf628534753443d469cdf") -- SHINOBI Art of Vengeance - SEGA Villains Stage - Depot 3381241
+-- SHINOBI Art of Vengeance - Digital Art Book and Original Soundtrack (AppID: 3381250)
+addappid(3381250)
+addappid(3381250, 1, "155147f9c285079d0c8de3a293bb7e0ebc4c5e59763e5eb6f6ddb5a14c37df25") -- SHINOBI Art of Vengeance - Digital Art Book and Original Soundtrack - Depot 3381250
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3381210) -- SHINOBI Art of Vengeance - SEGA Blue Outfit
+addappid(3381220) -- SHINOBI Art of Vengeance - Original Arcade Outfit and Fortune Hunter Amulet
+addappid(3381230) -- SHINOBI Art of Vengeance - Starter Pack
+addappid(3381260) -- SHINOBI Art of Vengeance Digital Deluxe Upgrade
