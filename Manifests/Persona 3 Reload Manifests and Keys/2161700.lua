a02da5474a14372e14e04f593c0f5b2f42b7b02f
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-16 21:15:58 +0200

    Update Persona 3 Reload Manifests and Keys/2161700.lua

---
+++ Manifests/Persona 3 Reload Manifests and Keys/2161700.lua
@@ -0,0 +1,36 @@
+-- 2161700's Lua and Manifest Created by Hubcap Manifest
+-- Persona 3 Reload
+-- Created: April 14, 2026 at 21:36:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 11
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2161700, 1, "bf1950cacab9ce218e10c58f74c39b508f1e5917aad3dc4107be9a6f0298cad7") -- Persona 3 Reload
+-- MAIN APP DEPOTS
+addappid(2161701, 1, "9644c57e48eb13144cd26fa9423b85051e1c474221f8f6cfcb017478cd8f3503") -- Depot 2161701
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Persona 3 Reload - Digital Artbook (AppID: 2517300)
+addappid(2517300)
+addappid(2517301, 1, "99c413d3350860f2c095f2948891bdaf234aff41e3de3ecdce62384d0db89c1e") -- Persona 3 Reload - Digital Artbook - Depot 2517301
+-- Persona 3 Reload - Digital Soundtrack (AppID: 2517310)
+addappid(2517310)
+addappid(2517311, 1, "6fe33390db2c2102f8745ea2cfe432836e4337a33b4e78ef71456aa4e37529d0") -- Persona 3 Reload - Digital Soundtrack - Depot 2517311
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2322840) -- Persona 3 Reload - Persona 4 Golden Yasogami High Costume Set
+addappid(2334530) -- Persona 3 Reload - Persona 5 Royal Shujin Academy Costume Set
+addappid(2334531) -- Persona 3 Reload - Persona 5 Royal Phantom Thieves Costume Set
+addappid(2334532) -- Persona 3 Reload - Persona 5 Royal Persona Set 1
+addappid(2334533) -- Persona 3 Reload - Persona 5 Royal Persona Set 2
+addappid(2334534) -- Persona 3 Reload - Persona 4 Golden Persona Set
+addappid(2334535) -- Persona 3 Reload - Persona 4 Golden BGM Set
+addappid(2334536) -- Persona 3 Reload - Persona 5 Royal BGM Set
+addappid(2689210) -- Persona 3 Reload Expansion Pass
+-- EMPTY DEPOTS (no content on any branch)
+-- addappid(2517300) -- Depot 2517300 (empty depot)
+-- addappid(2517310) -- Depot 2517310 (empty depot)
