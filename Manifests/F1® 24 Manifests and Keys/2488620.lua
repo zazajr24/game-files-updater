8f32b62819911fa2fab86549b9f549bf49d39748
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-20 13:16:20 +0200

    Update 2488620.lua

---
+++ Manifests/F1® 24 Manifests and Keys/2488620.lua
@@ -0,0 +1,30 @@
+-- 2488620's Lua and Manifest Created by Hubcap Manifest
+-- F1® 24
+-- Created: September 29, 2025 at 11:15:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 6
+-- Total DLCs: 11
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2488620, 1, "703777a80cf4d7e480beff412e25cdbdb698d1ec6fa6bc3ef1920437e590d6ce") -- F1® 24
+-- MAIN APP DEPOTS
+addappid(2488621, 1, "631754f71ffa45dd53c6375d1e093a58315b5c7468e7dae0b1c1f168b1e3b062") -- Depot 2488621
+addappid(2488622, 1, "3bb5b95c44afb22626dfe7f4717d1a8c829e93cf3bfad336352103c304cb4378") -- Depot 2488622
+addappid(2488623, 1, "e2f55053a31b74536dc064d80009d8fb3905a66be6c1c4809a522f27166f609c") -- Depot 2488623
+addappid(2488624, 1, "13515b2f09f7b81529a2c82c13692f6148e80f4aea11fde68100e35560e67a60") -- Depot 2488624
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2751140) -- F1 24 Champions Upgrade
+addappid(2751150) -- F1 24 Pre-Order Starter Pack
+addappid(2751160) -- F1 24 Champions Bumper Pack
+addappid(2751170) -- F1 24 Champions Icons Pack
+addappid(2751180) -- F1 24 Champions Pre-Order Pack
+addappid(2751190) -- F1 TV Pro 1-Month Subscription
+addappid(2791190) -- F1 24 Champions Icons Pack
+addappid(2807940) -- F1 24 Champions Bumper Pack
+addappid(2994700) -- F1 24 VIP Podium Pass Pack
+addappid(3075400) -- F1 24 PitCoin
+addappid(3335270) -- F1 25 Pre-Order Livery Pack
