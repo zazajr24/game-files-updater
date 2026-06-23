00746bd732d3bf52648f8bafe795015bf02581da
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 19:36:46 +0200

    Update 1462570.lua

---
+++ Manifests/Lost in Random™ Manifests and Keys/1462570.lua
@@ -0,0 +1,23 @@
+-- 1462570's Lua and Manifest Created by Hubcap Manifest
+-- Lost in Random™
+-- Created: June 22, 2026 at 19:36:24 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 9
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1462570, 1, "bd5ee431e3869a23b5e304363abf07969e99fdbec7d5c01bad9ac542fb29fb44") -- Lost in Random™
+-- MAIN APP DEPOTS
+addappid(1462571, 1, "d42eb343714c848a585b32e1d16f1fec8406b8596a0d50c66bd49bbcc7089c35") -- Lost In Random - Content
+addappid(1462572, 1, "e94a618db2c3fc1ac5278e81ebaba1620fb59f1e6ebaea3ca7c49267894de48c") -- Lost In Random - en_US
+addappid(1462573, 1, "0b7eb88b534fbf9b44b05194a14925460c1fd51d96a59f55efe21404c362cc65") -- Lost In Random - fr_FR
+addappid(1462574, 1, "04e21c8539faf5a8fda6f9c6c8bf270100ed2bdda1f86c89347426363ebed32d") -- Lost In Random - de_DE
+addappid(1462575, 1, "f01edcc130d4598a8777447ff940edaf1dc0500eb9953796b763cb945c4202c9") -- Lost In Random - it_IT
+addappid(1462576, 1, "21f5d99b180542e3ff06f618de2bf9b2870322c183049fd406cf0a333a5db164") -- Lost In Random - es_ES
+addappid(1462577, 1, "4daf71b2bc3a0520e515deb9acfc9933a6c5ddbe9f7a808a362b2eadb57653ac") -- Lost In Random - ja_JP
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1582400) -- Lost in Random - Key
