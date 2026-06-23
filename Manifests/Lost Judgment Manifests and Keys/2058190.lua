780787a6b6480ec446a412800386137dfb56e4bc
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 16:41:44 +0200

    Update 2058190.lua

---
+++ Manifests/Lost Judgment Manifests and Keys/2058190.lua
@@ -0,0 +1,26 @@
+-- 2058190's Lua and Manifest Created by Hubcap Manifest
+-- Lost Judgment
+-- Created: September 29, 2025 at 22:42:27 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 11
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2058190) -- Lost Judgment
+-- MAIN APP DEPOTS
+addappid(2058191, 1, "39536eee50189371b847b8c923772bb0a1914079459c852bac325c55f71bd085") -- Depot 2058191
+addappid(2058192, 1, "16020a20ac3bd8d42984905d72924d4923f6db53593fd874f5977dd3b81a5b29") -- Depot 2058192
+addappid(2058193, 1, "249f3e81ec2455f6556dcfe0bc2c582a57350c703a2c5018b4a35a52bc30e918") -- Depot 2058193
+addappid(2058194, 1, "41a42f75e89e1333aaae4d1f21cd1d40d79d573fb63179ea465f955419f86c31") -- Depot 2058194
+addappid(2058195, 1, "7ae46cfc89b26b8575d1ffe8656b2cd947d97782c34c7ffe72618ffc10f13a33") -- Depot 2058195
+addappid(2058196, 1, "732d3974d75cfc0e28b47bd192a69fda650dc05a8bf1f6ba52b7d8692a3d7983") -- Depot 2058196
+addappid(2058197, 1, "1479564412ea2e8db403c95327009a29fe07a89fc55ebb65576c763870154fa2") -- Depot 2058197
+addappid(2058198, 1, "4c49c509bbfb496da9e5d2a48ed82a3e308cf8cccbc6067b40d54a97f2cf7802") -- Depot 2058198
+addappid(2058199, 1, "0bfec0fce4a72ac9879db6e54968c03419b4318f4167daf097a5ea22baa97fb8") -- Depot 2058199
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Lost Judgment - The Kaito Files Story Expansion (AppID: 2060280)
+addappid(2060280)
+addappid(2060280, 1, "8f210ec51cad794270eddc55022b23a70e688e715909c17bf7778d3fe8ccef71") -- Lost Judgment - The Kaito Files Story Expansion - Depot 2060280
