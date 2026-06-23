15b920aded7ecc0e66eeac62eb7ee2c316331c12
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 19:32:54 +0200

    Update 1038250.lua

---
+++ Manifests/DIRT 5 Manifests and Keys/1038250.lua
@@ -0,0 +1,26 @@
+-- 1038250's Lua and Manifest Created by Hubcap Manifest
+-- DIRT 5
+-- Created: September 29, 2025 at 07:51:31 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 10
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1038250) -- DIRT 5
+-- MAIN APP DEPOTS
+addappid(1038251, 1, "43d22ea77f4f8585fe8af24877fe8922406632b6508c6adf08509fdae67d2e62") -- CP 2 Content
+addappid(1038252, 1, "8373feec31992e456a798b03476b51d4ebdb5b673a5405323ecfb1ea4dd777f8") -- CP2 exe
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1363540) -- DIRT 5 - Gameplay Booster Pack
+addappid(1363591) -- DIRT 5 - Porsche Macan T1 Rally Raid
+addappid(1379260) -- DIRT 5 - Year 1 Upgrade
+addappid(1379261) -- DIRT 5 - Ford F-150 Raptor PreRunner by Deberti Design
+addappid(1469470) -- DIRT 5 - Amplified Lanyard
+addappid(1469471) -- DIRT 5 - Year One Lanyard
+addappid(1516260) -- DIRT 5 - Energy Content Pack
+addappid(1517270) -- DIRT 5 - Uproar Content Pack
+addappid(1633610) -- DIRT 5 - Super Size Content Pack
+addappid(1690420) -- DIRT 5 - Wild Spirits Pack
