61da0a611acea5f15f2b761716f689bcc1b61038
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 19:37:43 +0200

    Update 1677350.lua

---
+++ Manifests/EA SPORTS™ PGA TOUR™ Manifests and Keys/1677350.lua
@@ -0,0 +1,24 @@
+-- 1677350's Lua and Manifest Created by Hubcap Manifest
+-- EA SPORTS™ PGA TOUR™
+-- Created: June 20, 2026 at 11:19:55 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 5
+-- Total DLCs: 5
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1677350, 1, "d24fca576c0a59a1b58567a4d9482ed185c53a3466816f92401c5d8a8b929ccd") -- EA SPORTS™ PGA TOUR™
+-- MAIN APP DEPOTS
+addappid(1677351, 1, "35926dbbb1966047e99f70059b05fc88298e874d0e6ca2b90bd37d5bf6e9028d") -- Depot 1677351
+addappid(1677352, 1, "36d9ae111d606396ade5b1ab43997daca111cf50de39988b97177baf9c13fc4b") -- Depot 1677352
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(3340991, 1, "023daedb070e5af8704dc88ee0af829f5c11923d2e6a42cca11ba5713b9f4491") -- Depot 3340991 (Shared from App 3340990)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1681650) -- Talladega - Standard Edition - Key
+addappid(2226200) -- PGA Tour - Deluxe Edition Key
+addtoken(2226200, "4069012734721830396")
+addappid(2226201) -- PGA Tour - Deluxe Edition Pre-Order Key
+addappid(2226202) -- PGA Tour - Standard Edition Pre-Order Key
+addappid(2337580) -- EA SPORTS PGA TOUR Deluxe Upgrade
