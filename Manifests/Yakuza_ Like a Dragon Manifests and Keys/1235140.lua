6369895bc194594bb919182e74a10e54ba8bc616
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 19:26:47 +0200

    Update 1235140.lua

---
+++ Manifests/Yakuza_ Like a Dragon Manifests and Keys/1235140.lua
@@ -0,0 +1,33 @@
+-- 1235140's Lua and Manifest Created by Hubcap Manifest
+-- Yakuza: Like a Dragon
+-- Created: October 01, 2025 at 08:40:54 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 7
+-- Total DLCs: 8
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1235140) -- Yakuza: Like a Dragon
+-- MAIN APP DEPOTS
+addappid(1235141, 1, "a6c3090851b8f41f9b628ed2b51335e1a9a6944cfb4041001860f1d10465adaa") -- yazawa Content
+addappid(1235142, 1, "ba44937f15fa93fd09f7e671c4927d11fc61e1d46a6c8140eea67eef77efbf0c") -- yazawa extend
+addappid(1235143, 1, "20bdc3f794349917d767fde04a451276f81c1ecbf0a5b9ee3757f34654cf8ff9") -- Yakuza extend2
+addappid(1235144, 1, "59e0edbfd783e05543bdd078b6c008f13cbeac80f621afbaed948cf86f554eb0") -- Yakuza_extend3
+-- SHARED DEPOTS (from other apps)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Yakuza Like a Dragon Pachislot Machines (AppID: 1261270)
+addappid(1261270)
+addappid(1261270, 1, "35065fd4cc1828f50c79b892de5c9a8ce97333c1ff4bfa2f8b7757ee63cc05ae") -- Yakuza Like a Dragon Pachislot Machines - Pachislots 4 Machines Free DLC Pack デポ
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1253350) -- Yakuza Like a Dragon Job Set
+addappid(1259670) -- Yakuza Like a Dragon - Crafting Mat Bundle
+addtoken(1259670, "18240685630471378090")
+addappid(1289190) -- Yakuza Like a Dragon Management Mode Set
+addappid(1289191) -- Yakuza Like a Dragon Ultimate Costume Set
+addappid(1289192) -- Yakuza Like a Dragon - Stat Boost Set
+addtoken(1289192, "6598235155635036293")
+addappid(1289211) -- Yakuza Like a Dragon Legends Costume Set
+addtoken(1289211, "5300469950141033188")
+addappid(1289234) -- Yakuza Like a Dragon Karaoke Set
