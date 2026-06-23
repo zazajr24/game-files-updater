f84d02e11ffcb07f53fc3aa476ba0f47503107f3
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 16:46:55 +0200

    Update 1844380.lua

---
+++ Manifests/Warhammer Age of Sigmar_ Realms of Ruin Manifests and Keys/1844380.lua
@@ -0,0 +1,22 @@
+-- 1844380's Lua and Manifest Created by Hubcap Manifest
+-- Warhammer Age of Sigmar: Realms of Ruin
+-- Created: October 01, 2025 at 06:34:38 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 6
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1844380) -- Warhammer Age of Sigmar: Realms of Ruin
+-- MAIN APP DEPOTS
+addappid(1844381, 1, "34ab542f6d7b2eb7aa9e7623c37b81259208343c7989509c7dc2e559405fa22d") -- Depot 1844381
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2531100) -- Warhammer Age of Sigmar Realms of Ruin - The Yndrasta, Celestial Spear Pack
+addappid(2531110) -- Warhammer Age of Sigmar Realms of Ruin - The Gobsprakk, The Mouth of Mork Pack
+addappid(2531120) -- Warhammer Age of Sigmar Realms of Ruin - Preorder Bonus
+addappid(2531130) -- Warhammer Age of Sigmar Realms of Ruin Deluxe Upgrade Pack
+addappid(2744270) -- Warhammer Age of Sigmar Realms of Ruin - Kurdoss Valentian, The Craven King 
+addappid(2744280) -- Warhammer Age of Sigmar Realms of Ruin - Gaunt Summoner
