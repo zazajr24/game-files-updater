ec026827bdd4129ed5516d4650f5c00123ea4679
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-23 19:28:17 +0200

    Update 2375550.lua

---
+++ Manifests/Like a Dragon Gaiden_ The Man Who Erased His Name Manifests and Keys/2375550.lua
@@ -0,0 +1,16 @@
+-- 2375550's Lua and Manifest Created by Hubcap Manifest
+-- Like a Dragon Gaiden: The Man Who Erased His Name
+-- Created: September 29, 2025 at 22:11:32 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2375550) -- Like a Dragon Gaiden: The Man Who Erased His Name
+-- MAIN APP DEPOTS
+addappid(2375551, 1, "98c80a1e378de4c0c534551cba0c53e74326cd8ff2f9360ad6fa4f5588a4729e") -- Depot 2375551
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2438750) -- Like a Dragon Gaiden The Man Who Erased His Name - Legendary Fighter Pack
