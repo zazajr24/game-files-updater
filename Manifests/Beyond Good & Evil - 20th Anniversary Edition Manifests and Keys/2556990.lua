e29dda851984a51122ef5f92bdb9c46d2a48c989
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-26 19:28:04 +0200

    Update 2556990.lua

---
+++ Manifests/Beyond Good & Evil - 20th Anniversary Edition Manifests and Keys/2556990.lua
@@ -0,0 +1,18 @@
+-- 2556990's Lua and Manifest Created by Hubcap Manifest
+-- Beyond Good and Evil - 20th Anniversary Edition
+-- Created: June 17, 2026 at 15:45:17 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2556990, 1, "e1d6a667dc03ebfd929d9dcb0e2f689e844ba3d6eb88ee2034833f3b5dbd894a") -- Beyond Good and Evil - 20th Anniversary Edition
+-- MAIN APP DEPOTS
+addappid(2556991, 1, "fcfb5884860aaf7a15a1a823f60814216d3bb2fdcac130608793ae365b774ac6") -- Depot 2556991
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675") -- Ubisoft Connect PC Client Content (Shared from App 1716750)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2691270) -- Beyond Good  Evil 20th Anniversary Edition Ubisoft Activation
