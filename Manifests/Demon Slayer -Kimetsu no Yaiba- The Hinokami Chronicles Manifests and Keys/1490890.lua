f4bbe7625d9efd131b32bd43cf1955d59dc850c6
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-29 21:21:38 +0200

    Update 1490890.lua

---
+++ Manifests/Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Manifests and Keys/1490890.lua
@@ -0,0 +1,56 @@
+-- 1490890's Lua and Manifest Created by Hubcap Manifest
+-- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles
+-- Created: October 05, 2025 at 08:13:50 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 10
+-- Total DLCs: 22
+-- Shared Depots: 2
+-- Blacklisted Depots: 1
+--   Depot 1490892: Only KR CN
+
+-- MAIN APPLICATION
+addappid(1490890) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles
+-- MAIN APP DEPOTS
+addappid(1490891, 1, "20196f9d842139359a0275126f03992d9510d5b96c4213c38ad6d95c6d72d402") -- Butterfly Content
+addappid(1490893, 1, "aff3930bfe0975c776a9dbe8c34b6b96167ab0db098e0764ba3b15b288ed1548") -- Caterpillar
+addappid(1490894, 1, "d2ec4b7a405fdbac76fa443457c80f5b157516d888a2c8abe599a50033cf2e32") -- Larva
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITH DEDICATED DEPOTS
+-- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles  Character Avatar (Final Selection Tanjiro) (AppID: 1760470)
+addappid(1760470)
+addappid(1760470, 1, "70a688482c965ca7753eff82322f589bf14c05bcc6e641908f54ab17610a84b0") -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles  Character Avatar (Final Selection Tanjiro) - Kimono
+-- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Character avatars x 12 (full roster) (AppID: 1760480)
+addappid(1760480)
+addappid(1760480, 1, "82dca9e50950f252b389747c8f4e33a4754f34fd7d8e93b2d8a013532b6a4d0e") -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Character avatars x 12 (full roster) - Yukata
+-- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles  High-res PC Wallpaper (Previously Steam profile background) (AppID: 1760490)
+addappid(1760490)
+addappid(1760490, 1, "c5e244432a04f26054022a4f38c1419c2c743df9b79d9a706bdf38cf2fb41856") -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles  High-res PC Wallpaper (Previously Steam profile background) - Fusuma
+-- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Character avatars x 10 (full roster) (AppID: 1760500)
+addappid(1760500)
+addappid(1760500, 1, "07cfb0664655d5544016861463b28175d000e7880ecaea791cc4739c26f8069f") -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Character avatars x 10 (full roster) - Hakama
+-- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Avatar 2 Pack (AppID: 1777140)
+addappid(1777140)
+addappid(1777140, 1, "010c8f072b3fc9a3f7b089be2e56dedc947bc221f9bf5ee6e98bb09bc6d0b032") -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Avatar 2 Pack - Depot 1777140
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1669200) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Kimetsu Academy Tanjiro Early Unlock
+addappid(1669201) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Kimetsu Academy Nezuko, Zenitsu, Inosuke Early Unlock
+addappid(1669202) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Kimetsu Academy Giyu, Shinobu Early Unlock
+addappid(1669203) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Butterfly Mansion Patient Wear for Tanjiro, Zenitsu  Inosuke
+addappid(1669204) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles 8,000 Kimetsu Points
+addappid(1769970) -- Demon Slayer -Kimetsu No Yaiba- The Hinokami Chronicles Core Add-on Bundle
+addappid(1769971) -- Demon Slayer -Kimetsu No Yaiba- The Hinokami Chronicles Costume Bundle
+addappid(1977370) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Kimetsu Academy Summer Uniforms
+addappid(1977380) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Tengen Uzui Character Pack
+addappid(1977381) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Tanjiro, Zenitsu,  Inosuke (Entertainment District) Character Pack
+addappid(1981880) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Character Pass
+addappid(1981881) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Nezuko (Advanced Demon Form) Character Pack
+addappid(1981882) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Tanjiro (Entertainment District) Character Pack
+addtoken(1981882, "2549071661430823736")
+addappid(1981883) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Daki Character Pack
+addappid(1981884) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Gyutaro Character Pack
+addappid(1981930) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Zenitsu (Entertainment District) Character Pack
+addtoken(1981930, "2439409664835297523")
+addappid(1996370) -- Demon Slayer -Kimetsu no Yaiba- The Hinokami Chronicles Inosuke (Entertainment District) Character Pack
+addtoken(1996370, "14421256256447768348")
