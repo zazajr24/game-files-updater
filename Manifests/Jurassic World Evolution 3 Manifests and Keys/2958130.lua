81c12b7728a1716d1896748c6fa9e1ea67e58c11
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-29 21:22:26 +0200

    Update 2958130.lua

---
+++ Manifests/Jurassic World Evolution 3 Manifests and Keys/2958130.lua
@@ -0,0 +1,19 @@
+-- 2958130's Lua and Manifest Created by Hubcap Manifest
+-- Jurassic World Evolution 3
+-- Created: June 29, 2026 at 06:00:44 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 2
+-- Total DLCs: 4
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2958130, 1, "28c45829d1af12623b4fead679e384291bfb040d0241fc6be9d05f7d4cdbbd62") -- Jurassic World Evolution 3
+-- MAIN APP DEPOTS
+addappid(2958131, 1, "dc7cb4cb8d7d76fdcab84fed34fa1f799e7459cf1be5ed041116ed846211889d") -- Depot 2958131
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3651190) -- Jurassic World Evolution 3 Badlands Set
+addappid(3651200) -- Jurassic World Evolution 3 Deluxe Upgrade Pack
+addappid(4131660) -- Jurassic World Evolution 3 Wetlands
+addappid(4131670) -- Jurassic World Evolution 3 Rebirth Expansion
