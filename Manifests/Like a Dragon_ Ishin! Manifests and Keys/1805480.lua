7f6a1636dc9090d43bb9dde2e62752edbb23a7ba
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-29 21:20:57 +0200

    Update 1805480.lua

---
+++ Manifests/Like a Dragon_ Ishin! Manifests and Keys/1805480.lua
@@ -0,0 +1,27 @@
+-- 1805480's Lua and Manifest Created by Hubcap Manifest
+-- Like a Dragon: Ishin!
+-- Created: September 29, 2025 at 22:11:33 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 11
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1805480) -- Like a Dragon: Ishin!
+-- MAIN APP DEPOTS
+addappid(1805481, 1, "b6865a3777287943bedd9ec78a50d638f0eea4d711441035e656bcc633a83765") -- Depot 1805481
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2104730) -- Like a Dragon Ishin - Shinsengumi Captains Set
+addappid(2104731) -- Like a Dragon Ishin - Ryoma Growth Support Kit
+addappid(2104732) -- Like a Dragon Ishin - Sword Upgrade Materials Kit
+addappid(2104733) -- Like a Dragon Ishin - Gun Upgrade Materials Kit
+addappid(2104734) -- Like a Dragon Ishin - Third Division Armament Expansion Kit
+addappid(2104735) -- Like a Dragon Ishin - Dragon of Dojima Skin
+addappid(2104736) -- Like a Dragon Ishin - Japanese Swords  Cannon Set (Pre-Purchase)
+addappid(2163170) -- Like a Dragon Ishin - Bonechewer Katana
+addappid(2163171) -- Like a Dragon Ishin - Waterdrop Katana
+addappid(2202540) -- Like a Dragon Ishin - Elite Generals Trooper Card Bundle
+addappid(2223050) -- Like a Dragon Ishin - Dragons Clutch Katana
