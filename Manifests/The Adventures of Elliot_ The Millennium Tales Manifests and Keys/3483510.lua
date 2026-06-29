62dc75e8b679e9ef0088b9e2a178e88b48cdadba
ZazaJr24 <kaanleon2462@gmail.com>
2026-06-29 21:20:40 +0200

    Update 3483510.lua

---
+++ Manifests/The Adventures of Elliot_ The Millennium Tales Manifests and Keys/3483510.lua
@@ -0,0 +1,18 @@
+-- 3483510's Lua and Manifest Created by Hubcap Manifest
+-- The Adventures of Elliot: The Millennium Tales
+-- Created: June 19, 2026 at 12:39:05 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 2
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3483510, 1, "a17d2e0760baf6b66e3ed3253087b686cf224be40f3975685c6115c67b8bfc0e") -- The Adventures of Elliot: The Millennium Tales
+-- MAIN APP DEPOTS
+addappid(3483511, 1, "ee7fd8a235ebc8f1301e13d7f127fe3a64d1ea1c15fcd9ef6385d2e3285b52f3") -- Depot 3483511
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4031020) -- The Adventures of Elliot The Millennium Tales Digital Deluxe Upgrade
+addappid(4031030) -- Elliots Departure Pack
